674b6f1f9d9b83a5f3d50d6cba9ce2cc022a0158
brodsom <brodsom@users.sourceforge.net>
2002-11-13 02:58:14 +0000

    solution to times/dates problems with milliseconds and jdk 1.3 /1.4

--- src/main/org/firebirdsql/gds/XSQLVAR.java
+++ src/main/org/firebirdsql/gds/XSQLVAR.java
@@ -126,129 +126,201 @@ public class XSQLVAR {
     }
 
     public final static byte[] encodeTimestamp(java.sql.Timestamp value){
-        // some versions of JDK (1.4.1 win) return millis with getTime() when
-        // api docs say it only returns integral seconds 
-        // we have to take millis out and use getNanos for every version to work OK 
-        long millis = (value.getTime()/1000)*1000 + value.getNanos() / 1000000;
-        
+
         // note, we cannot simply pass millis to the database, because
         // Firebird stores timestamp in format (citing Ann W. Harrison):
         //
         // "[timestamp is] stored a two long words, one representing 
         // the number of days since 17 Nov 1858 and one representing number 
         // of 100 nano-seconds since midnight"
-        
-        byte[] date = encodeDate(new java.sql.Date(millis));
-        byte[] time = encodeTime(new java.sql.Time(millis));
-        
-        byte[] result = new byte[date.length + time.length];
-        System.arraycopy(date, 0, result, 0, date.length);
-        System.arraycopy(time, 0, result, date.length, time.length);
-        
+        datetime d = new datetime(value);
+
+        byte[] date = d.toDateBytes();
+        byte[] time = d.toTimeBytes();
+
+        byte[] result = new byte[8];
+        System.arraycopy(date, 0, result, 0, 4);
+        System.arraycopy(time, 0, result, 4, 4);
+
         return result;
     }
 
     public final static java.sql.Timestamp decodeTimestamp(byte[] byte_int){
-        // return new java.sql.Timestamp(decodeLong(byte_int));
         
         if (byte_int.length != 8)
             throw new IllegalArgumentException("Bad parameter to decode");
-        
+
         // we have to extract time and date correctly
         // see encodeTimestamp(...) for explanations
-            
+
         byte[] date = new byte[4];
         byte[] time = new byte[4];
         
-        System.arraycopy(byte_int, 0, date, 0, date.length);
-        System.arraycopy(byte_int, date.length, time, 0, time.length);
-        
-        return new java.sql.Timestamp(
-            decodeDate(date).getTime() + 
-            decodeTime(time).getTime());
-    } 
-	 
+        System.arraycopy(byte_int, 0, date, 0, 4);
+        System.arraycopy(byte_int, 4, time, 0, 4);
+
+        datetime d = new datetime(date,time);
+        return d.toTimestamp();
+    }
+
     public final static byte[] encodeTime(java.sql.Time d) {
-        GregorianCalendar c = new GregorianCalendar();
-        c.setTime(d);
-        long millisInDay =
-          c.get(Calendar.HOUR_OF_DAY)*60*60*1000
-          +
-          c.get(Calendar.MINUTE)*60*1000
-          +
-          c.get(Calendar.SECOND)*1000
-          +
-          c.get(Calendar.MILLISECOND)
-          ;
-        int saving_offset = c.get(Calendar.DST_OFFSET);
-        int zone_offset = c.get(Calendar.ZONE_OFFSET);
-        int iTime = (int) (millisInDay * 10);
-        return encodeInt(iTime);
+        datetime dt = new datetime(d);
+        return dt.toTimeBytes();
     }
 
     public final static java.sql.Time decodeTime(byte[] int_byte) {
-        GregorianCalendar c = new GregorianCalendar();
-        int millisInDay = decodeInt(int_byte)/10;
-        java.sql.Time time = new java.sql.Time(millisInDay); //-c.get(Calendar.ZONE_OFFSET));
-        return time;		  
+        datetime dt = new datetime(null,int_byte);
+        return dt.toTime();
     }
-	 
+
     public final static byte[] encodeDate(java.util.Date d) {
-        int day, month, year;
-        int c, ya;
-
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTime(d);
-
-        day = calendar.get(Calendar.DAY_OF_MONTH);
-        month = calendar.get(Calendar.MONTH) + 1;
-        year = calendar.get(Calendar.YEAR);
-
-        if (month > 2) {
-            month -= 3;
-        } else {
-            month += 9;
-            year -= 1;
-        }
-
-        c = year / 100;
-        ya = year - 100 * c;
-
-        int value = ((146097 * c) / 4 +
-                 (1461 * ya) / 4 +
-                 (153 * month + 2) / 5 +
-                 day + 1721119 - 2400001);
-        return encodeInt(value);
+        datetime dt = new datetime(d);
+        return dt.toDateBytes();
     }
 
-    public final static java.sql.Date decodeDate(byte[] byte_int) {        		 
-        int sql_date = decodeInt(byte_int);
+    public final static java.sql.Date decodeDate(byte[] byte_int) {
+        datetime dt = new datetime(byte_int, null);
+        return dt.toDate();
+    }
 
-        int year, month, day, century;
+	 //
+	 // Helper Class to encode/decode times/dates
+	 //
+	 private static class datetime{
+        int year;
+        int month;
+        int day;
+        int hour;
+        int minute;
+        int second;
+        int millisecond;
 
-        sql_date -= 1721119 - 2400001;
-        century = (4 * sql_date - 1) / 146097;
-        sql_date = 4 * sql_date - 1 - 146097 * century;
-        day = sql_date / 4;
-
-        sql_date = (4 * day + 3) / 1461;
-        day = 4 * day + 3 - 1461 * sql_date;
-        day = (day + 4) / 4;
-
-        month = (5 * day - 3) / 153;
-        day = 5 * day - 3 - 153 * month;
-        day = (day + 5) / 5;
-
-        year = 100 * century + sql_date;
-
-        if (month < 10) {
-            month += 3;
-        } else {
-            month -= 9;
-            year += 1;
+        datetime(java.sql.Timestamp value){
+            Calendar c = new GregorianCalendar();
+            c.setTime(value);
+            year = c.get(Calendar.YEAR);
+            month = c.get(Calendar.MONTH)+1;
+            day = c.get(Calendar.DAY_OF_MONTH);
+            hour = c.get(Calendar.HOUR);
+            minute = c.get(Calendar.MINUTE);
+            second = c.get(Calendar.SECOND);
+            millisecond = value.getNanos()/1000000;
         }
 
-        Calendar calendar = new GregorianCalendar(year, month - 1, day);
-        return new java.sql.Date(calendar.getTime().getTime());
+        datetime(java.util.Date value){
+            Calendar c = new GregorianCalendar();
+            c.setTime(value);
+            year = c.get(Calendar.YEAR);
+            month = c.get(Calendar.MONTH)+1;
+            day = c.get(Calendar.DAY_OF_MONTH);
+            hour = 0;
+            minute = 0;
+            second = 0;
+            millisecond = 0;
+        }
+
+        datetime(java.sql.Time value){
+            Calendar c = new GregorianCalendar();
+            c.setTime(value);
+            year = 0;
+            month = 0;
+            day = 0;
+            hour = c.get(Calendar.HOUR);
+            minute = c.get(Calendar.MINUTE);
+            second = c.get(Calendar.SECOND);
+            millisecond = c.get(Calendar.MILLISECOND);
+        }
+
+        datetime(byte[] date, byte[] time){
+
+            if (date != null){
+                int sql_date = decodeInt(date);
+                int century;
+                sql_date -= 1721119 - 2400001;
+                century = (4 * sql_date - 1) / 146097;
+                sql_date = 4 * sql_date - 1 - 146097 * century;
+                day = sql_date / 4;
+
+                sql_date = (4 * day + 3) / 1461;
+                day = 4 * day + 3 - 1461 * sql_date;
+                day = (day + 4) / 4;
+
+                month = (5 * day - 3) / 153;
+                day = 5 * day - 3 - 153 * month;
+                day = (day + 5) / 5;
+
+                year = 100 * century + sql_date;
+
+                if (month < 10) {
+                    month += 3;
+                } else {
+                    month -= 9;
+                    year += 1;
+                }
+            }
+            if (time != null){		
+                int millisInDay = decodeInt(time)/10;
+                hour = millisInDay / 3600000;
+                minute = (millisInDay - hour*3600000) / 60000;
+                second = (millisInDay - hour*3600000 - minute * 60000) / 1000;
+                millisecond = millisInDay - hour*3600000 - minute * 60000 - second * 1000;
+            }
+        }
+
+        byte[] toTimeBytes(){
+            int millisInDay = (hour * 3600000 + minute * 60000 + second * 1000 + millisecond)*10; 
+            return encodeInt(millisInDay);
+        }
+
+        byte[] toDateBytes(){
+            int cpMonth = month;
+            int cpYear = year;
+            int c, ya;
+
+            if (cpMonth > 2) {
+                cpMonth -= 3;
+            } else {
+                cpMonth += 9;
+                cpYear -= 1;
+            }
+
+            c = cpYear / 100;
+            ya = cpYear - 100 * c;
+
+            int value = ((146097 * c) / 4 +
+                 (1461 * ya) / 4 +
+                 (153 * cpMonth + 2) / 5 +
+                 day + 1721119 - 2400001);
+            return encodeInt(value);
+        }
+
+        java.sql.Time toTime(){
+            Calendar c = new GregorianCalendar();
+            c.set(Calendar.HOUR,hour);
+            c.set(Calendar.MINUTE,minute);
+            c.set(Calendar.SECOND,second);
+            c.set(Calendar.MILLISECOND,millisecond);			 
+            return new java.sql.Time(c.getTime().getTime());
+        }
+
+        java.sql.Timestamp toTimestamp(){
+            Calendar c = new GregorianCalendar();
+            c.set(Calendar.YEAR,year);
+            c.set(Calendar.MONTH,month-1);
+            c.set(Calendar.DAY_OF_MONTH,day);
+            c.set(Calendar.HOUR,hour);
+            c.set(Calendar.MINUTE,minute);
+            c.set(Calendar.SECOND,second);
+            c.set(Calendar.MILLISECOND,millisecond);
+            return new java.sql.Timestamp(c.getTime().getTime());
+        }
+
+        java.sql.Date toDate(){
+            Calendar c = new GregorianCalendar();
+            c.set(Calendar.YEAR,year);
+            c.set(Calendar.MONTH,month-1);
+            c.set(Calendar.DAY_OF_MONTH,day);
+            return new java.sql.Date(c.getTime().getTime());
+        }
     }
 }
