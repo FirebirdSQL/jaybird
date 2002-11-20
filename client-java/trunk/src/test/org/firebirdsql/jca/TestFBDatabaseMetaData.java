38e1c43d8c3428cfc3456b55e91b8497db9a3155
d_jencks <d_jencks@65644016-39b1-43b1-bf79-96bc8fe82c15>
2002-11-20 15:04:24 +0000

    fix test from changed typeinfo count

--- client-java/trunk/src/test/org/firebirdsql/jca/TestFBDatabaseMetaData.java
+++ client-java/trunk/src/test/org/firebirdsql/jca/TestFBDatabaseMetaData.java
@@ -508,7 +508,7 @@ public class TestFBDatabaseMetaData extends TestXABase {
             out += System.getProperty("line.separator");
         }
         if (log != null) log.info("getTypeInfoblePrivileges returned: " + out);
-        assertTrue("Not enough TypeInfo rows fetched: " + count, count >= 16);
+        assertTrue("Not enough TypeInfo rows fetched: " + count, count >= 15);
     }
 
 
