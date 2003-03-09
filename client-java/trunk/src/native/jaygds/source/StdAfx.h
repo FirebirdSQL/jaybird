06d05a129ba588619075a6571f66c76a65ea58ef
ryanbaldwin <ryanbaldwin@65644016-39b1-43b1-bf79-96bc8fe82c15>
2003-03-09 15:00:58 +0000

    Added native code for type 2 driver modifications.

---
+++ client-java/trunk/src/native/jaygds/source/StdAfx.h
@@ -0,0 +1,43 @@
+/*
+ * Firebird Open Source J2ee connector - jdbc driver
+ *
+ * Distributable under LGPL license.
+ * You may obtain a copy of the License at http://www.gnu.org/copyleft/lgpl.html
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * LGPL License for more details.
+ *
+ * This file was created by members of the firebird development team.
+ * All individual contributions remain the Copyright (C) of those
+ * individuals.  Contributors to this file are either listed here or
+ * can be obtained from a CVS history command.
+ *
+ * All rights reserved.
+ */
+
+// stdafx.h : include file for standard system include files,
+//  or project specific include files that are used frequently, but
+//      are changed infrequently
+//
+
+#if !defined(AFX_STDAFX_H__5F99A579_2606_4470_AAFE_5921F6EC9270__INCLUDED_)
+#define AFX_STDAFX_H__5F99A579_2606_4470_AAFE_5921F6EC9270__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+
+// Insert your headers here
+#define WIN32_LEAN_AND_MEAN		// Exclude rarely-used stuff from Windows headers
+
+#include <windows.h>
+
+// TODO: reference additional headers your program requires here
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_STDAFX_H__5F99A579_2606_4470_AAFE_5921F6EC9270__INCLUDED_)
