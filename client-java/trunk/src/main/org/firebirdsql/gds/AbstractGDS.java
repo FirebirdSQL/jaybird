1516ba75b6243c84bf7fd21c8522e5354b575eba
ryanbaldwin <ryanbaldwin@65644016-39b1-43b1-bf79-96bc8fe82c15>
2003-07-27 19:47:32 +0000

    Added missing file headers and one or two additional comments.

--- client-java/trunk/src/main/org/firebirdsql/gds/AbstractGDS.java
+++ client-java/trunk/src/main/org/firebirdsql/gds/AbstractGDS.java
@@ -1,3 +1,22 @@
+/*
+ * Firebird Open Source J2ee connector - jdbc driver
+ *
+ * Distributable under LGPL license.
+ * You may obtain a copy of the License at http://www.gnu.org/copyleft/lgpl.html
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * LGPL License for more details.
+ *
+ * This file was created by members of the firebird development team.
+ * All individual contributions remain the Copyright (C) of those
+ * individuals.  Contributors to this file are either listed here or
+ * can be obtained from a CVS history command.
+ *
+ * All rights reserved.
+ */
+
 package org.firebirdsql.gds;
 
 import java.io.Externalizable;
@@ -6,22 +25,21 @@ import java.io.IOException;
 import java.io.ObjectInput;
 
 /**
- * Created by IntelliJ IDEA.
- * User: Ryan Baldwin
- * Date: 26-May-2003
- * Time: 02:14:22
- * To change this template use Options | File Templates.
+ * Base class for GDS implementations. This base class allows the GDS implementation
+ * to be serialized and deserialized safeley.
+ *
  */
 public abstract class AbstractGDS implements GDS, Externalizable
 	{
 	public AbstractGDS()
 		{
-
 		}
+
 	public AbstractGDS(GDSFactory.GdsType gdsType)
 		{
 		this.gdsType = gdsType;
 		}
+
 
 	public void writeExternal(ObjectOutput out) throws IOException
 		{
--- client-java/trunk/src/main/org/firebirdsql/ngds/GDS_Impl.java
+++ client-java/trunk/src/main/org/firebirdsql/ngds/GDS_Impl.java
@@ -1,10 +1,22 @@
-/**
- * Created by IntelliJ IDEA.
- * User: Ryan Baldwin
- * Date: Feb 10, 2003
- * Time: 2:24:53 AM
- * To change this template use Options | File Templates.
+/*
+ * Firebird Open Source J2ee connector - jdbc driver
+ *
+ * Distributable under LGPL license.
+ * You may obtain a copy of the License at http://www.gnu.org/copyleft/lgpl.html
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * LGPL License for more details.
+ *
+ * This file was created by members of the firebird development team.
+ * All individual contributions remain the Copyright (C) of those
+ * individuals.  Contributors to this file are either listed here or
+ * can be obtained from a CVS history command.
+ *
+ * All rights reserved.
  */
+
 package org.firebirdsql.ngds;
 
 import org.firebirdsql.gds.*;
@@ -17,6 +29,11 @@ import java.io.InputStream;
 import java.io.FileOutputStream;
 import java.io.File;
 
+
+/**
+ * Type 2 GDS implementation.
+ *
+ */
 public class GDS_Impl extends AbstractGDS implements GDS
     {
     private static Logger log = LoggerFactory.getLogger(GDS_Impl.class,false);
