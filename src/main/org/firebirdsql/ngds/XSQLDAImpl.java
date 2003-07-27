83ded5fcbbcdb9e9918a2a496637e182221753cf
ryanbaldwin <ryanbaldwin@users.sourceforge.net>
2003-07-27 19:47:32 +0000

    Added missing file headers and one or two additional comments.

--- src/main/org/firebirdsql/ngds/XSQLDAImpl.java
+++ src/main/org/firebirdsql/ngds/XSQLDAImpl.java
@@ -1,9 +1,20 @@
-/**
- * Created by IntelliJ IDEA.
- * User: Ryan Baldwin
- * Date: Feb 18, 2003
- * Time: 11:05:20 PM
- * To change this template use Options | File Templates.
+/*
+ * Firebird Open Source J2ee connector - jdbc driver
+ *
+ * Distributable under LGPL license.
+ * You may obtain a copy of the License at http://www.gnu.org/copyleft/lgpl.html
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * LGPL License for more details.
+ *
+ * This file was created by members of the firebird development team.
+ * All individual contributions remain the Copyright (C) of those
+ * individuals.  Contributors to this file are either listed here or
+ * can be obtained from a CVS history command.
+ *
+ * All rights reserved.
  */
 package org.firebirdsql.ngds;
 
