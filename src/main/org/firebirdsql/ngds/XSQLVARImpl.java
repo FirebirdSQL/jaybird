c23e46275576c33c77f5526e1a4485d16d70ee72
ryanbaldwin <ryanbaldwin@users.sourceforge.net>
2003-11-15 23:08:29 +0000

    Applied 'fixed bug in time decoding' too ngds package.

--- src/main/org/firebirdsql/ngds/XSQLVARImpl.java
+++ src/main/org/firebirdsql/ngds/XSQLVARImpl.java
@@ -394,6 +394,9 @@ public class XSQLVARImpl extends XSQLVAR
 
         java.sql.Time toTime(){
             Calendar c = new GregorianCalendar();
+		    c.set(Calendar.YEAR, 1970);
+            c.set(Calendar.MONTH, Calendar.JANUARY);
+            c.set(Calendar.DAY_OF_MONTH, 1);
             c.set(Calendar.HOUR_OF_DAY,hour);
             c.set(Calendar.MINUTE,minute);
             c.set(Calendar.SECOND,second);
