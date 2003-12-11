6383debf23fe91dde71d6683e7d53a1e80310609
rrokytskyy <rrokytskyy@users.sourceforge.net>
2003-12-11 13:11:15 +0000

    added test case to check SQLState

--- src/test/org/firebirdsql/jdbc/TestFBDriver.java
+++ src/test/org/firebirdsql/jdbc/TestFBDriver.java
@@ -17,79 +17,10 @@
  * All rights reserved.
  */
 
-
-
-/*
-
- * CVS modification log:
-
- * $Log$
- * Revision 1.10  2003/06/13 23:44:01  rrokytskyy
- * fixed problem in warning test
- * ----------------------------------------------------------------------
- *
- * Revision 1.9  2003/06/05 23:40:46  brodsom
- * Substitute package and inline imports
- *
- * Revision 1.8  2003/06/04 13:51:01  brodsom
- * Remove unused vars and imports
- *
- * Revision 1.7  2002/12/12 23:32:21  rrokytskyy
- * removed unnecessary stack trace printing
- *
- * Revision 1.6  2002/11/20 15:04:57  d_jencks
- * Demonstrate problem with timestamps
- *
- * Revision 1.5  2002/11/19 17:37:47  d_jencks
- * verify that DECIMAL(18,0) stores all Long values.
- *
- * Revision 1.4  2002/11/15 00:13:15  rrokytskyy
- * fixed bug with committing transaction when connection is closed
- *
- * Revision 1.3  2002/10/18 14:22:26  rrokytskyy
- * fixed warnings handling
- *
- * Revision 1.2  2002/08/29 13:41:16  d_jencks
- * Changed to lgpl only license.  Moved driver to subdirectory to make build system more consistent.
- *
- * Revision 1.1  2002/08/14 13:22:46  d_jencks
- * Moved tests to separate directory. Removed need for jmx classes, and removed jmxri.jar
- *
- * Revision 1.7  2002/06/06 11:24:07  brodsom
- * Performance patch. Log if log4j is in the classpath, don't log if the enviroment variable FBLog4j is false.
- *
- * Revision 1.6  2002/02/02 18:58:24  d_jencks
- * converted to log4j logging and cleaned up some test problems.  If you do not wish to use log4j, you may leave out the log4j-core.jar and get no logging
- *
- * Revision 1.5  2002/01/07 06:59:54  d_jencks
- * Revised FBManager to create dialect 3 databases, and the tests to use a newly created database. Simplified and unified test constants. Test targets are now all-tests for all tests and one-test for one test: specify the test as -Dtest=Gds one-test for the TestGds.class test.  Made a few other small changes to improve error messages
- *
- * Revision 1.4  2002/01/06 23:37:58  d_jencks
- * added a connection test to datasource test, cleaned up constants a bit.
- *
- * Revision 1.3  2001/08/28 17:13:23  d_jencks
- * Improved formatting slightly, removed dos cr's
- *
- * Revision 1.2  2001/07/15 21:17:36  d_jencks
- * Updated to use assertTrue rather than assert, for junit 3.7
- *
- * Revision 1.1  2001/07/13 18:16:15  d_jencks
- * Implementation of jdbc 1.0 Driver contributed by Roman Rokytskyy
- *
-
- * Revision 1.1  2001/07/09 09:09:51  rrokytskyy
-
- * Initial revision
-
- *
-
- */
-
-
-
 package org.firebirdsql.jdbc;
 
 
+import java.sql.*;
 import java.sql.Connection;
 import java.sql.Driver;
 import java.sql.DriverManager;
@@ -203,6 +134,27 @@ public class TestFBDriver extends FBTestBase {
 	        dialect3Connection.close();
         }
     }
+    
+    public void testGetSQLState() throws Exception {
+        Connection connection = getConnectionViaDriverManager();
+        
+        try {
+            Statement stmt = connection.createStatement();
+            try {
+                stmt.executeQuery("select * from");
+            } catch(SQLException ex) {
+                
+                assertTrue("getSQLState() method does not returns String Value",
+                        ex.getSQLState() instanceof java.lang.String);
+                
+            } finally {
+                stmt.close();
+            }
+        } finally {
+            connection.close();
+        }
+    }
+    
 
 
     public void testLongRange() throws Exception
