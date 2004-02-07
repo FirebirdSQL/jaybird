68afd02ac64cd82f208f216275544527f9d048d2
ryanbaldwin <ryanbaldwin@65644016-39b1-43b1-bf79-96bc8fe82c15>
2004-02-07 11:04:53 +0000

    Removed references to a number of unused entry points.

--- client-java/trunk/src/native/jaygds/source/fb_binding.h
+++ client-java/trunk/src/native/jaygds/source/fb_binding.h
@@ -32,60 +32,6 @@ typedef ISC_STATUS ISC_EXPORT prototype_isc_attach_database(ISC_STATUS *,
 										  short,
 										  char *);
 
-typedef ISC_STATUS ISC_EXPORT prototype_isc_array_gen_sdl(ISC_STATUS *,
-										ISC_ARRAY_DESC *,
-										short *,
-										char *,
-										short *);
-
-typedef ISC_STATUS ISC_EXPORT prototype_isc_array_get_slice(ISC_STATUS *,
-										  isc_db_handle *,
-										  isc_tr_handle *,
-										  ISC_QUAD *,
-										  ISC_ARRAY_DESC *,
-										  void *,
-										  ISC_LONG *);
-
-typedef ISC_STATUS ISC_EXPORT prototype_isc_array_lookup_bounds(ISC_STATUS *,
-											  isc_db_handle *,
-											  isc_tr_handle *,
-											  char *,
-											  char *,
-											  ISC_ARRAY_DESC *);
-
-typedef ISC_STATUS ISC_EXPORT prototype_isc_array_lookup_desc(ISC_STATUS *,
-											isc_db_handle *,
-											isc_tr_handle *,
-											char *,
-											char *,
-											ISC_ARRAY_DESC *);
-
-typedef ISC_STATUS ISC_EXPORT prototype_isc_array_set_desc(ISC_STATUS *,
-										 char *,
-										 char *,
-										 short *,
-										 short *,
-										 short *,
-										 ISC_ARRAY_DESC *);
-
-typedef ISC_STATUS ISC_EXPORT prototype_isc_array_put_slice(ISC_STATUS *,
-										  isc_db_handle *,
-										  isc_tr_handle *,
-										  ISC_QUAD *,
-										  ISC_ARRAY_DESC *,
-										  void *,
-										  ISC_LONG *);
-
-typedef void ISC_EXPORT prototype_isc_blob_default_desc(ISC_BLOB_DESC *,
-									  unsigned char *,
-									  unsigned char *);
-
-typedef ISC_STATUS ISC_EXPORT prototype_isc_blob_gen_bpb(ISC_STATUS *,
-									   ISC_BLOB_DESC *,
-									   ISC_BLOB_DESC *,
-									   unsigned short,
-									   unsigned char *,
-									   unsigned short *);
 
 typedef ISC_STATUS ISC_EXPORT prototype_isc_blob_info(ISC_STATUS *,
 									isc_blob_handle *,
@@ -94,28 +40,10 @@ typedef ISC_STATUS ISC_EXPORT prototype_isc_blob_info(ISC_STATUS *,
 									short,
 									char *);
 
-typedef ISC_STATUS ISC_EXPORT prototype_isc_blob_lookup_desc(ISC_STATUS *,
-										   isc_db_handle *,
-										   isc_tr_handle *,
-										   unsigned char *,
-										   unsigned char *,
-										   ISC_BLOB_DESC *,
-										   unsigned char *);
-
-typedef ISC_STATUS ISC_EXPORT prototype_isc_blob_set_desc(ISC_STATUS *,
-										unsigned char *,
-										unsigned char *,
-										short,
-										short,
-										short,
-										ISC_BLOB_DESC *);
 
 typedef ISC_STATUS ISC_EXPORT prototype_isc_cancel_blob(ISC_STATUS *,
 									  isc_blob_handle *);
 
-typedef ISC_STATUS ISC_EXPORT prototype_isc_cancel_events(ISC_STATUS *,
-										isc_db_handle *,
-										ISC_LONG *);
 
 typedef ISC_STATUS ISC_EXPORT prototype_isc_close_blob(ISC_STATUS *,
 									 isc_blob_handle *);
@@ -269,26 +197,8 @@ typedef void ISC_EXPORT prototype_isc_encode_sql_time(void *,
 typedef void ISC_EXPORT prototype_isc_encode_timestamp(void *,
 									 ISC_TIMESTAMP *);
 
-typedef ISC_LONG ISC_EXPORT_VARARG prototype_isc_event_block(char * *,
-										   char * *,
-										   unsigned short, ...);
 
-typedef void ISC_EXPORT prototype_isc_event_counts(ISC_ULONG *,
-								 short,
-								 char *,
-								 char *);
 
-/* 17 May 2001 - isc_expand_dpb is DEPRECATED */
-typedef void ISC_EXPORT_VARARG prototype_isc_expand_dpb(char * *,
-									  short *, ...);
-
-typedef int ISC_EXPORT prototype_isc_modify_dpb(char * *,
-							  short *,
-							  unsigned short,
-							  char *,
-							  short);
-
-typedef ISC_LONG ISC_EXPORT prototype_isc_free(char *);
 
 typedef ISC_STATUS ISC_EXPORT prototype_isc_get_segment(ISC_STATUS *,
 									  isc_blob_handle *,
@@ -296,17 +206,6 @@ typedef ISC_STATUS ISC_EXPORT prototype_isc_get_segment(ISC_STATUS *,
 									  unsigned short,
 									  char *);
 
-typedef ISC_STATUS ISC_EXPORT prototype_isc_get_slice(ISC_STATUS *,
-									isc_db_handle *,
-									isc_tr_handle *,
-									ISC_QUAD *,
-									short,
-									char *,
-									short,
-									ISC_LONG *,
-									ISC_LONG,
-									void *,
-									ISC_LONG *);
 
 typedef ISC_STATUS ISC_EXPORT prototype_isc_interprete(char *,
 									 ISC_STATUS * *);
@@ -351,16 +250,10 @@ typedef ISC_STATUS ISC_EXPORT prototype_isc_put_slice(ISC_STATUS *,
 									ISC_LONG,
 									void *);
 
-typedef ISC_STATUS ISC_EXPORT prototype_isc_que_events(ISC_STATUS *,
-									 isc_db_handle *,
-									 ISC_LONG *,
-									 ISC_USHORT,
-									 ISC_UCHAR *,
-									 isc_callback,
-									 void *);
 
 typedef ISC_STATUS ISC_EXPORT prototype_isc_rollback_retaining(ISC_STATUS *,
 											 isc_tr_handle *);
+
 
 typedef ISC_STATUS ISC_EXPORT prototype_isc_rollback_transaction(ISC_STATUS *,
 											   isc_tr_handle *);
@@ -380,28 +273,8 @@ typedef void ISC_EXPORT prototype_isc_sql_interprete(short,
 								   char *,
 								   short);
 
-typedef ISC_STATUS ISC_EXPORT prototype_isc_transaction_info(ISC_STATUS *,
-										   isc_tr_handle *,
-										   short,
-										   char *,
-										   short,
-										   char *);
 
-typedef ISC_STATUS ISC_EXPORT prototype_isc_transact_request(ISC_STATUS *,
-										   isc_db_handle *,
-										   isc_tr_handle *,
-										   unsigned short,
-										   char *,
-										   unsigned short,
-										   char *,
-										   unsigned short,
-										   char *);
 
-typedef ISC_LONG ISC_EXPORT prototype_isc_vax_integer(char *,
-									short);
-
-typedef ISC_INT64 ISC_EXPORT prototype_isc_portable_integer(unsigned char *,
-										  short);
 
 typedef ISC_STATUS ISC_EXPORT prototype_isc_seek_blob(ISC_STATUS *,
 									isc_blob_handle *,
@@ -454,23 +327,9 @@ class FirebirdApiBinding
 			
 		static bool		IsLoaded();
 	
-		
-		
-
 		static prototype_isc_attach_database*		isc_attach_database;
-		static prototype_isc_array_gen_sdl*			isc_array_gen_sdl;
-		static prototype_isc_array_get_slice*		isc_array_get_slice;
-		static prototype_isc_array_lookup_bounds*	isc_array_lookup_bounds;
-		static prototype_isc_array_lookup_desc*		isc_array_lookup_desc;
-		static prototype_isc_array_set_desc*		isc_array_set_desc;
-		static prototype_isc_array_put_slice*		isc_array_put_slice;
-		static prototype_isc_blob_default_desc*		isc_blob_default_desc;
-		static prototype_isc_blob_gen_bpb*			isc_blob_gen_bpb;
 		static prototype_isc_blob_info*				isc_blob_info;
-		static prototype_isc_blob_lookup_desc*		isc_blob_lookup_desc;
-		static prototype_isc_blob_set_desc*			isc_blob_set_desc;
 		static prototype_isc_cancel_blob*			isc_cancel_blob;
-		static prototype_isc_cancel_events*			isc_cancel_events;
 		static prototype_isc_close_blob*			isc_close_blob;
 		static prototype_isc_commit_retaining*		isc_commit_retaining;
 		static prototype_isc_commit_transaction*	isc_commit_transaction;
@@ -503,13 +362,7 @@ class FirebirdApiBinding
 		static prototype_isc_encode_sql_date*		isc_encode_sql_date;
 		static prototype_isc_encode_sql_time*		isc_encode_sql_time;
 		static prototype_isc_encode_timestamp*		isc_encode_timestamp;
-		static prototype_isc_event_block*			isc_event_block;
-		static prototype_isc_event_counts*			isc_event_counts;
-		static prototype_isc_expand_dpb*			isc_expand_dpb;
-		static prototype_isc_modify_dpb*			isc_modify_dpb;
-		static prototype_isc_free*					isc_free;
 		static prototype_isc_get_segment*			isc_get_segment;
-		static prototype_isc_get_slice*				isc_get_slice;
 		static prototype_isc_interprete*			isc_interprete;
 		static prototype_isc_open_blob*				isc_open_blob;
 		static prototype_isc_open_blob2*			isc_open_blob2;
@@ -517,17 +370,11 @@ class FirebirdApiBinding
 		static prototype_isc_print_sqlerror*		isc_print_sqlerror;
 		static prototype_isc_print_status*			isc_print_status;
 		static prototype_isc_put_segment*			isc_put_segment;
-		static prototype_isc_put_slice*				isc_put_slice;
-		static prototype_isc_que_events*			isc_que_events;
 		static prototype_isc_rollback_retaining*	isc_rollback_retaining;
 		static prototype_isc_rollback_transaction*	isc_rollback_transaction;
-		static prototype_isc_start_multiple*		isc_start_multiple;
 		static prototype_isc_start_transaction*		isc_start_transaction;
 		static prototype_isc_sqlcode*				isc_sqlcode;
 		static prototype_isc_sql_interprete*		isc_sql_interprete;
-		static prototype_isc_transaction_info*		isc_transaction_info;
-		static prototype_isc_transact_request*		isc_transact_request;
-		static prototype_isc_vax_integer*			isc_vax_integer;
 		static prototype_isc_seek_blob*				isc_seek_blob;
 		static prototype_isc_service_attach*			isc_service_attach;
 		static prototype_isc_service_detach*			isc_service_detach;
