1e734c6876333a9ae5b434b6eae3698f85d03405
rrokytskyy <rrokytskyy@users.sourceforge.net>
2004-05-28 22:58:13 +0000

    exported few properties, updated javadoc

--- src/pool/org/firebirdsql/pool/FBWrappingDataSource.java
+++ src/pool/org/firebirdsql/pool/FBWrappingDataSource.java
@@ -36,45 +36,115 @@ import org.firebirdsql.jdbc.FBConnectionHelper;
  * Implementation of {@link javax.sql.DataSource} including connection pooling.
  * Following properties are supported:
  * <ul>
- * <li><code>blobBufferSize</code> size of the buffer used to transfer BLOB data.
- * <li><code>blockingTimeout</code> time in milliseconds during which 
- * {@link #getConnection()} method will block if no free connection is in pool.
- * <li><code>database</code> path to a database including the server name;
- * for example <code>localhost/3050:c:/path/to/database.gdb</code>.
- * <li><code>encoding</code> character encoding for the JDBC connection.
- * <li><code>idleTimeout</code> time in milliseconds after which 
- * idle physical connection in the pool is closed.
- * <li><code>loginTimeout</code> property from {@link javax.sql.DataSource},
- * in this context is a synonym for <code>blockingTimeout</code> (however
- * value is specified in seconds).
- * <li><code>maxSize</code> maximum number of physical connections that can
- * be opened by this data source.
- * <li><code>minSize</code> minimum number of connections that will remain open
- * by this data source.
- * <li><code>password</code> password that is used to connect to database.
- * <li><code>pingInterval</code> time interval during which connection will
- * be proved for aliveness.
- * <li><code>socketBufferSize</code> size of the socket buffer in bytes. In some 
- * cases values used by JVM by default are not optimal. This results in 
- * performance degradation (especially when you transfer big BLOBs). Usually 
- * 8192 bytes provides good results.
- * <li><code>sqlRole</code> SQL role name.
- * <li><code>tpbMapping</code> mapping of the TPB parameters to JDBC transaction
- * isolation levels.
- * <li><code>type</code> type of connection that will be created. There are 
- * three possible types: pure Java (or type 4), type 2 that will use Firebird
- * client library to connect to the database, and embedded that will use 
- * embedded engine (access to local databases). Possible values are (case 
- * insensitive):
- * <ul> 
- * <li><code>"PURE_JAVA"</code> or <code>"TYPE4"</code> for pure Java (type 4) 
- * JDBC connections;
- * <li><code>"NATIVE"</code> or <code>"TYPE2"</code> to use Firebird client
- * library;
- * <li><code>"EMBEDDED"</code> to use embedded engine.
- * </ul>
- * <li><code>userName</code> name of the user that will be used to access the 
- * database.
+ * <li><code>blobBufferSize</code> 
+ *      size of the buffer used to transfer BLOB data.
+ * 
+ * <li><code>blockingTimeout</code> 
+ *      time in milliseconds during which {@link #getConnection()} method will 
+ *      block if no free connection is in pool.
+ * 
+ * <li><code>charSet</code>
+ *      similar to <code>encoding</code>, but takes Java character set name
+ *      instead of Firebird's encoding.
+ * 
+ * <li><code>database</code> 
+ *      path to a database including the server name; for example 
+ *      <code>localhost/3050:c:/path/to/database.gdb</code>.
+ * 
+ * <li><code>encoding</code> 
+ *      character encoding for the JDBC connection.
+ * 
+ * <li><code>freeSize</code>
+ *      read-only: gives amount of free connections in the pool, when 0, blocking
+ *      will occur if <code>workingSize</code> is equal to <code>maxPoolSize</code>.
+ * 
+ * <li><code>isolation</code>
+ *      default transaction isolation level for connections as string; possible
+ *      values are:
+ *      <ul>
+ *      <li>TRANSACTION_READ_COMMITTED
+ *      <li>TRANSACTION_REPEATABLE_READ
+ *      <li>TRANSACTION_SERIALIZABLE
+ * 
+ * <li><code>loginTimeout</code> 
+ *      property from {@link javax.sql.DataSource}, in this context is a synonym 
+ *      for <code>blockingTimeout</code> (however value is specified in seconds).
+ * 
+ * <li><code>maxIdleTime</code> 
+ *      time in milliseconds after which idle physical connection in the 
+ *      pool is closed.
+ * 
+ * <li><code>maxStatements</code>
+ *      maximum number of pooled prepared statements, if 0, pooling is switched
+ *      off.
+ * 
+ * <li><code>maxPoolSize</code> 
+ *      maximum number of physical connections that can be opened by this data 
+ *      source.
+ * 
+ * <li><code>minPoolSize</code> 
+ *      minimum number of connections that will remain open by this data source.
+ * 
+ * <li><code>nonStandardProperty</code>
+ *      a non-standard connection parameter in form <code>name[=value]</code>.
+ * 
+ * <li><code>password</code> 
+ *      password that is used to connect to database.
+ * 
+ * <li><code>pingInterval</code> 
+ *      time interval during which connection will be proved for aliveness.
+ * 
+ * <li><code>pooling</code>
+ *      allows switching pooling off.
+ * 
+ * <li><code>statementPooling</code>
+ *      alternative way to switch statement pooling off.
+ * 
+ * <li><code>socketBufferSize</code> 
+ *      size of the socket buffer in bytes. In some cases values used by JVM by 
+ *      default are not optimal. This results in performance degradation 
+ *      (especially when you transfer big BLOBs). Usually 8192 bytes provides 
+ *      good results.
+ * 
+ * <li><code>sqlRole</code> 
+ *      SQL role name.
+ * 
+ * <li><code>tpbMapping</code> 
+ *      mapping of the TPB parameters to JDBC transaction isolation levels.
+ * 
+ * <li><code>transactionIsolationLevel</code>
+ *      default transaction isolation level, number from {@link java.sql.Connection}
+ *      interface.
+ * 
+ * <li><code>totalSize</code>
+ *      total number of allocated connections.
+ * 
+ * <li><code>type</code> 
+ *      type of connection that will be created. There are four possible types: 
+ *      pure Java (or type 4), type 2 that will use Firebird client library to 
+ *      connect to the database, local-mode type 2 driver, and embedded that 
+ *      will use embedded engine (access to local databases). Possible values 
+ *      are (case insensitive):
+ *      <ul> 
+ *      <li><code>"PURE_JAVA"</code> or <code>"TYPE4"</code> 
+ *          for pure Java (type 4) JDBC connections;
+ * 
+ *      <li><code>"NATIVE"</code> or <code>"TYPE2"</code> 
+ *          to use Firebird client library;
+ * 
+ *      <li><code>"LOCAL"</code> 
+ *          to use Firebird client library in local-mode (IPC link to server);
+ * 
+ *      <li><code>"EMBEDDED"</code> 
+ *          to use embedded engine.
+ *      </ul>
+ * 
+ * <li><code>userName</code> 
+ *      name of the user that will be used to access the database.
+ * 
+ * <li><code>workingSize</code>
+ *      number of connections that are in use (e.g. were obtained using
+ *      {@link #getConnection()} method, but not yet closed).
  * </ul>
  * 
  * @author <a href="mailto:rrokytskyy@users.sourceforge.net">Roman Rokytskyy</a>
@@ -224,18 +294,54 @@ public class FBWrappingDataSource implements DataSource,
     	setEncoding(iscEncoding);
     }
 
+    public int getMaxIdleTime() {
+        return getPool().getMaxIdleTime();
+    }
+    
+    public void setMaxIdleTime(int maxIdleTime) {
+        getPool().setMaxIdleTimeout(maxIdleTime);
+    }
+    
+    /**
+     * @deprecated non-standard name, use {@link #getMaxIdleTime()}.
+     */
     public int getIdleTimeout() {
         return getPool().getIdleTimeout();
     }
 
+    /**
+     * @deprecated non-standard name, use {@link #setMaxIdleTime()}.
+     */
     public void setIdleTimeout(int idleTimeoutValue) {
         getPool().setIdleTimeout(idleTimeoutValue);
     }
 
+    public int getMaxStatements() {
+        return getPool().getMaxStatements();
+    }
+    
+    public void setMaxStatements(int maxStatements) {
+        getPool().setMaxStatements(maxStatements);
+    }
+    
+    public int getMaxPoolSize() {
+        return getPool().getMaxPoolSize();
+    }
+    
+    public void setMaxPoolSize(int maxPoolSize) {
+        getPool().setMaxPoolSize(maxPoolSize);
+    }
+    
+    /**
+     * @deprecated non-standard name, use {@link #getMaxPoolSize()}.
+     */
     public int getMaxConnections() {
         return getPool().getMaxConnections();
     }
     
+    /**
+     * @deprecated non-standard name, use {@link #setMaxPoolSize(int)}.
+     */
     public void setMaxConnections(int maxConnections) {
         getPool().setMaxConnections(maxConnections);
     }
@@ -353,8 +459,23 @@ public class FBWrappingDataSource implements DataSource,
         getPool().setStatementPooling(statementPooling);
     }
     
+    /**
+     * @deprecated Confusing name. Use {@link #getFreeSize()} instead.
+     */
     public int getConnectionCount() throws SQLException {
         return getPool().getFreeSize();
+    }
+    
+    public int getFreeSize() throws SQLException {
+        return getPool().getFreeSize();
+    }
+    
+    public int getWorkingSize() throws SQLException {
+        return getPool().getWorkingSize();
+    }
+    
+    public int getTotalSize() throws SQLException {
+        return getPool().getTotalSize();
     }
     
     public int getTransactionIsolationLevel() {
