6e83341984c679947c1b4ade30e3ee7278c7c155
rrokytskyy <rrokytskyy@65644016-39b1-43b1-bf79-96bc8fe82c15>
2004-10-03 19:41:02 +0000

    removed support for isc_api.dll as deprecated

--- client-java/trunk/src/main/org/firebirdsql/ngds/GDS_Impl.java
+++ client-java/trunk/src/main/org/firebirdsql/ngds/GDS_Impl.java
@@ -106,8 +106,6 @@ public class GDS_Impl extends AbstractGDS implements GDS
      };
 
     private static final String[] LIST_OF_ORACLE_MODE_LIBRARIES_TO_TRY = {
-        "isc_api.dll",
-        "libisc_api.so",
         "fyracle.dll",
         "libfyracle.so"
     };
