fb5b34352e3c70760b0d27a242db9ec3403c9e6a
rrokytskyy <rrokytskyy@65644016-39b1-43b1-bf79-96bc8fe82c15>
2004-10-08 22:30:57 +0000

    removed CVS history from file

--- client-java/trunk/src/main/org/firebirdsql/gds/GDSExceptionHelper.java
+++ client-java/trunk/src/main/org/firebirdsql/gds/GDSExceptionHelper.java
@@ -17,49 +17,6 @@
  * All rights reserved.
  */
 
-/*
- * CVS modification log:
- * $Log$
- * Revision 1.4  2004/08/22 00:52:28  rrokytskyy
- * fixed bug when the driver is loaded by the system classloader
- *
- * Revision 1.3  2003/06/05 22:57:47  brodsom
- * Substitute package and inline imports
- *
- * Revision 1.2  2003/03/09 16:30:48  rrokytskyy
- * removed some deprecations
- *
- * Revision 1.1  2002/08/29 13:41:01  d_jencks
- * Changed to lgpl only license.  Moved driver to subdirectory to make build system more consistent.
- *
- * Revision 1.9  2002/06/10 18:47:40  brodsom
- * logging change, logging depends on the first class used, default to true for FBManagedConnectionFactory, FBManager and tests and false for other classes.
- *
- * Revision 1.8  2002/06/06 11:24:07  brodsom
- * Performance patch. Log if log4j is in the classpath, don't log if the enviroment variable FBLog4j is false.
- *
- * Revision 1.7  2002/06/04 01:17:49  brodsom
- * performance patches
- *
- * Revision 1.6  2002/02/02 18:58:24  d_jencks
- * converted to log4j logging and cleaned up some test problems.  If you do not wish to use log4j, you may leave out the log4j-core.jar and get no logging
- *
- * Revision 1.5  2002/02/01 03:58:06  d_jencks
- * applied fix from William Suroweic in case we messed up the params in error messages
- *
- * Revision 1.4  2002/01/07 06:59:54  d_jencks
- * Revised FBManager to create dialect 3 databases, and the tests to use a newly created database. Simplified and unified test constants. Test targets are now all-tests for all tests and one-test for one test: specify the test as -Dtest=Gds one-test for the TestGds.class test.  Made a few other small changes to improve error messages
- *
- * Revision 1.3  2001/10/24 16:59:43  alberola
- * Fixed bug in ClassLoader
- *
- * Revision 1.2  2001/07/18 20:07:31  d_jencks
- * Added better GDSExceptions, new NativeSQL, and CallableStatement test from Roman Rokytskyy
- *
- * Revision 1.1  2001/07/16 03:57:43  d_jencks
- * added text error messages to GDSExceptions, thanks Roman Rokytskyy
- *
- */
 package org.firebirdsql.gds;
 
 import org.firebirdsql.logging.Logger;
@@ -82,7 +39,7 @@ public class GDSExceptionHelper {
 
    private static final Logger log = LoggerFactory.getLogger(GDSExceptionHelper.class,false);
 
-    private static final String MESSAGES = "/isc_error_msg";
+    private static final String MESSAGES = "isc_error_msg";
     private static Properties messages = new Properties();
 
     private static boolean initialized = false;
@@ -93,7 +50,7 @@ public class GDSExceptionHelper {
      */
     private static void init() {
         try {
-            String res = MESSAGES.replace('.','/') + ".properties";
+            String res = "/" + MESSAGES.replace('.','/') + ".properties";
 			InputStream in = GDSException.class.getResourceAsStream(res);
             
             if (in == null) {
