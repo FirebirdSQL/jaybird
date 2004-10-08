004bb7b66ed62089be61a7db7b6e1b6e78735506
rrokytskyy <rrokytskyy@users.sourceforge.net>
2004-10-08 22:43:29 +0000

    removed unused code and added LGPL license

--- src/main/org/firebirdsql/encodings/CharacterTranslator.java
+++ src/main/org/firebirdsql/encodings/CharacterTranslator.java
@@ -1,7 +1,23 @@
+/*
+ * Firebird Open Source J2ee connector - jdbc driver
+ *
+ * Distributable under LGPL license.
+ * You may obtain a copy of the License at http://www.gnu.org/copyleft/lgpl.html
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * LGPL License for more details.
+ *
+ * This file was created by members of the firebird development team.
+ * All individual contributions remain the Copyright (C) of those
+ * individuals.  Contributors to this file are either listed here or
+ * can be obtained from a CVS history command.
+ *
+ * All rights reserved.
+ */
 package org.firebirdsql.encodings;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.sql.SQLException;
 import java.util.Enumeration;
 import java.util.Iterator;
@@ -96,24 +112,5 @@ public class CharacterTranslator {
             
             mapping[dbChar] = javaChar;
         }
-    }
-    
-    private InputStream getMappingStream(String mappingPath) throws IOException {
-        
-        ClassLoader cl = getClass().getClassLoader();
-        if (cl == null)
-            cl = ClassLoader.getSystemClassLoader();
-        
-        InputStream in = cl.getResourceAsStream(mappingPath);
-        
-        if (in == null) {
-            cl = Thread.currentThread().getContextClassLoader();
-            in = cl.getResourceAsStream(mappingPath);
-        }
-        
-        if (in == null && !mappingPath.startsWith("/"))
-            in = getMappingStream("/" + mappingPath);
-
-        return in;
     }
 }
