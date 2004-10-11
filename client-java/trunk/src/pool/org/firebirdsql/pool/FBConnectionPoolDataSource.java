3b02cc18e17e4d04af135aa736533fac9daf55a4
rrokytskyy <rrokytskyy@65644016-39b1-43b1-bf79-96bc8fe82c15>
2004-10-11 21:03:19 +0000

    fixed memory leak

--- client-java/trunk/src/pool/org/firebirdsql/pool/FBConnectionPoolDataSource.java
+++ client-java/trunk/src/pool/org/firebirdsql/pool/FBConnectionPoolDataSource.java
@@ -440,6 +440,18 @@ public class FBConnectionPoolDataSource extends BasicAbstractConnectionPool
             
         pooledObjectReleased(event);
     }
+
+    /**
+     * Notify about the deallocation of the physical connection.
+     * 
+     * @param connectionEvent instance of {@link ConnectionEvent}.
+     */
+    public void physicalConnectionDeallocated(ConnectionEvent connectionEvent) {
+        PooledObjectEvent event = 
+            new PooledObjectEvent(connectionEvent.getSource(), true);
+        
+        physicalConnectionDeallocated(event);
+    }
     
 	/**
 	 * Notify about serious error when using the connection. Currently
