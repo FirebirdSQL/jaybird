41861d336752a61ba4ef3dfc7b162cfc6f91fe9e
rrokytskyy <rrokytskyy@users.sourceforge.net>
2004-10-11 06:43:43 +0000

    merged with HEAD

--- src/main/org/firebirdsql/gds/GDSSynchronizationPolicy.java
+++ src/main/org/firebirdsql/gds/GDSSynchronizationPolicy.java
@@ -22,7 +22,7 @@ import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
-import java.util.ArrayList;
+import java.util.HashSet;
 
 
 /**
@@ -77,7 +77,7 @@ public class GDSSynchronizationPolicy {
      * @return array of all implemented interfaces.
      */
     private static Class[] getAllInterfaces(Class clazz) {
-        ArrayList result = new ArrayList();
+        HashSet result = new HashSet();
         
         do {
             Class[] interfaces = clazz.getInterfaces();
