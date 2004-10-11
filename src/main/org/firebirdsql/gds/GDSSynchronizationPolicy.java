563a17a6c9763decbec11ce738b26c954cd743b6
rrokytskyy <rrokytskyy@users.sourceforge.net>
2004-10-11 06:42:03 +0000

    removed unused import

--- src/main/org/firebirdsql/gds/GDSSynchronizationPolicy.java
+++ src/main/org/firebirdsql/gds/GDSSynchronizationPolicy.java
@@ -22,7 +22,6 @@ import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
-import java.util.ArrayList;
 import java.util.HashSet;
 
 
