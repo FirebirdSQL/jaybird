74e7c0bd4cc5f4f36e2dae09f8a0bd9fc52d8872
gab_reid <gab_reid@users.sourceforge.net>
2005-01-10 10:04:33 +0000

    Cleanup, and added some tests to reflect changes in the BackupManager interface

--- src/test/org/firebirdsql/management/TestBackupManager.java
+++ src/test/org/firebirdsql/management/TestBackupManager.java
@@ -1,6 +1,7 @@
 package org.firebirdsql.management;
 
 import java.sql.Connection;
+import java.sql.Statement;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
@@ -13,6 +14,15 @@ import org.firebirdsql.gds.GDSType;
  */
 public class TestBackupManager extends FBTestBase {
 
+        
+    private BackupManager backupManager;
+
+    private FBManager fbManager;
+
+    private static final String TEST_TABLE = "CREATE TABLE TEST (A INT)";
+
+
+
     /**
      * @param name
      */
@@ -20,7 +30,6 @@ public class TestBackupManager extends FBTestBase {
         super(name);
     }
 
-    private FBManager fbManager;
     
     protected void setUp() throws Exception {
         super.setUp();
@@ -31,14 +40,24 @@ public class TestBackupManager extends FBTestBase {
 
         fbManager.setForceCreate(true);
         fbManager.createDatabase(getDatabasePath(), DB_USER, DB_PASSWORD);
-        
+
+        GDSType gdsType = GDSType.getType(System.getProperty("test.gds_type"));
+        backupManager = new FBBackupManager(gdsType);
+        backupManager.setHost("localhost");
+        backupManager.setUser("SYSDBA");
+        backupManager.setPassword("masterkey");
+        backupManager.setDatabase(getDatabasePath());
+        backupManager.setBackupPath(getBackupPath());
+        backupManager.setLogger(System.out);
+        backupManager.setVerbose(true);
     }
+
     private String getDatabasePath() {
-        return "." + DB_PATH + "/" + DB_NAME + ".fdb";
+        return  DB_PATH + "/" + DB_NAME;
     }
     
     private String getBackupPath() {
-        return "." + DB_PATH + "/" + DB_NAME + ".fbk";
+        return DB_PATH + "/" + DB_NAME + ".fbk";
     }
     
     private Connection getConnection() throws SQLException {
@@ -52,24 +71,28 @@ public class TestBackupManager extends FBTestBase {
         }
     }
 
+    private void createTestTable() throws SQLException {
+        Connection conn = getConnection();
+        Statement stmt = conn.createStatement();
+        stmt.executeUpdate(TEST_TABLE);
+        stmt.close();
+        conn.close();
+    }
+
     protected void tearDown() throws Exception {
-        fbManager.dropDatabase(getDatabasePath(), DB_USER, DB_PASSWORD);
-        fbManager.stop();
+        try { 
+            fbManager.dropDatabase(getDatabasePath(), DB_USER, DB_PASSWORD);
+            fbManager.stop();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
         
         super.tearDown();
     }
     
     public void testBackup() throws Exception {
-        BackupManager backupManager = new FBBackupManager(GDSType.PURE_JAVA);
-        backupManager.setHost("localhost");
-        backupManager.setUser("SYSDBA");
-        backupManager.setPassword("masterkey");
         
-        
-        backupManager.setDatabase(getDatabasePath());
-        backupManager.setBackupPath(getBackupPath());
-        backupManager.setLogger(System.out);
-        backupManager.backupDatabase(true);
+        backupManager.backupDatabase();
         
         fbManager.dropDatabase(getDatabasePath(), DB_USER, DB_PASSWORD);
         
@@ -80,12 +103,84 @@ public class TestBackupManager extends FBTestBase {
         } catch(SQLException ex) {
             // ignore
         }
-        
+
         System.out.println();
-        
-        backupManager.restoreDatabase(true);
+        backupManager.restoreDatabase();
         
         Connection c = getConnection();
         c.close();
     }
+
+    public void testSetBadBufferCount() {
+        try {
+            backupManager.setRestorePageBufferCount(-1);
+            fail("Page buffer count must be a positive value");
+        } catch (IllegalArgumentException e){
+            // Ignore
+        }
+        backupManager.setRestorePageBufferCount(500);
+    }
+
+    public void testSetBadPageSize() {
+        try {
+            backupManager.setRestorePageSize(4000);
+            fail("Page size must be one of 1024, 2048, 4196 or 8192)");
+        } catch (IllegalArgumentException e) {
+            // Ignore
+        }
+        backupManager.setRestorePageSize(4196);
+    }
+
+    public void testRestoreReadOnly() throws Exception {
+        createTestTable();
+        Connection conn = null;
+        try {
+            conn = getConnection();
+            Statement stmt = conn.createStatement();
+            stmt.executeUpdate("INSERT INTO TEST VALUES (1)");
+            conn.close();
+
+            backupManager.backupDatabase();
+            fbManager.dropDatabase(getDatabasePath(), DB_USER, DB_PASSWORD);
+            backupManager.setRestoreReadOnly(true);
+            backupManager.restoreDatabase();
+
+            conn = getConnection();
+            stmt = conn.createStatement();
+            try {
+                stmt.executeUpdate("INSERT INTO TEST VALUES (2)");
+                fail("Not possible to insert data in a read-only database");
+            } catch (SQLException e){
+                // Ignore
+            }
+
+            conn.close();
+
+            backupManager.setRestoreReadOnly(false);
+            backupManager.setRestoreCreate(false);
+            backupManager.restoreDatabase();
+            conn = getConnection();
+            stmt = conn.createStatement();
+            stmt.executeUpdate("INSERT INTO TEST VALUES (3)");
+        } finally {
+            if (conn != null){
+                conn.close();
+            }
+        }
+     }
+
+    public void testBackupReplace() throws Exception {
+        backupManager.backupDatabase();
+        backupManager.setRestoreCreate(true);
+        try {
+            backupManager.restoreDatabase();
+            fail("Can't restore-create an existing database");
+        } catch (SQLException e){
+            // Ignore
+        }
+        
+        backupManager.setRestoreCreate(false);
+        backupManager.restoreDatabase();
+    }
+
 }
