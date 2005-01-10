921da2e6c9525202659f0982abc0bf28ccbb4ac5
gab_reid <gab_reid@users.sourceforge.net>
2005-01-10 09:57:33 +0000

    Updated to reflect the refactoring changes in the BackupManager interface

--- src/main/org/firebirdsql/management/FBBackupManager.java
+++ src/main/org/firebirdsql/management/FBBackupManager.java
@@ -20,16 +20,12 @@
  */
 package org.firebirdsql.management;
 
-import java.io.IOException;
 import java.sql.SQLException;
 
 import org.firebirdsql.gds.GDS;
-import org.firebirdsql.gds.GDSException;
 import org.firebirdsql.gds.GDSType;
 import org.firebirdsql.gds.ISCConstants;
 import org.firebirdsql.gds.ServiceRequestBuffer;
-import org.firebirdsql.gds.isc_svc_handle;
-import org.firebirdsql.jdbc.FBSQLException;
 
 /**
  * Implements the backup and restore functionality of Firebird Services API.
@@ -41,94 +37,83 @@ public class FBBackupManager extends FBServiceManager implements BackupManager {
 
     private String backupPath;
 
+    private boolean verbose;
+
+    private int restoreBufferCount;
+
+    private int restorePageSize;
+
+    private boolean restoreReadOnly;
+
+    private boolean restoreCreate;
+
+
+    private static final int RESTORE_REPLACE = ISCConstants.isc_spb_res_replace;
+
+    private static final int RESTORE_CREATE = ISCConstants.isc_spb_res_create;
+
+
     /**
      * Create instance of this class.
      */
     public FBBackupManager(GDSType gdsType) {
         super(gdsType);
+
+        verbose = false;
+        restoreBufferCount = -1;
+        restorePageSize = -1;
+        restoreReadOnly = false;
+        restoreCreate = true;
     }
 
-    /*
-     * (non-Javadoc)
-     * 
+    /**
      * @see org.firebirdsql.management.BackupManager#getBackupPath()
      */
     public String getBackupPath() {
         return backupPath;
     }
 
-    /*
-     * (non-Javadoc)
-     * 
+    /**
      * @see org.firebirdsql.management.BackupManager#setBackupPath(java.lang.String)
      */
     public void setBackupPath(String backupPath) {
         this.backupPath = backupPath;
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.firebirdsql.management.BackupManager#backupDatabase(boolean)
+    /**
+     * @see org.firebirdsql.management.BackupManager#backupDatabase()
      */
-    public void backupDatabase(boolean verbose) throws SQLException,
-            IOException {
-        backupDatabase(0, verbose);
+    public void backupDatabase() throws SQLException {
+        backupDatabase(0);
     }
 
-    /*
-     * (non-Javadoc)
-     * 
+    /**
      * @see org.firebirdsql.management.BackupManager#backupMetadata(boolean)
      */
-    public void backupMetadata(boolean verbose) throws SQLException,
-            IOException {
-        backupDatabase(BACKUP_METADATA_ONLY, verbose);
+    public void backupMetadata() throws SQLException {
+        backupDatabase(BACKUP_METADATA_ONLY);
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.firebirdsql.management.BackupManager#backupDatabase(int,
-     *      boolean)
+    /**
+     * @see org.firebirdsql.management.BackupManager#backupDatabase(int)
      */
-    public void backupDatabase(int options, boolean verbose)
-            throws SQLException, IOException {
-
-        GDS gds = getGds();
-
-        try {
-            isc_svc_handle handle = attachServiceManager(gds);
-
-            try {
-                ServiceRequestBuffer backupSRB = getBackupSRB(options, verbose,
-                        gds);
-                gds.isc_service_start(handle, backupSRB);
-
-                queueService(gds, handle);
-            } finally {
-                detachServiceManager(gds, handle);
-            }
-        } catch (GDSException ex) {
-            throw new FBSQLException(ex);
-        }
+    public void backupDatabase(int options ) throws SQLException {
+        executeServicesOperation(getBackupSRB(options));
     }
 
     /**
      * Creates and returns the "backup" service request buffer for the Service
      * Manager.
      * 
-     * @param options
-     * @param verbose
-     * @param gds
+     * @param options The isc_spb_bkp_* parameters options to be used
      * @return the "backup" service request buffer for the Service Manager.
      */
-    private ServiceRequestBuffer getBackupSRB(int options, boolean verbose,
-            GDS gds) {
-        ServiceRequestBuffer backupSPB = gds
-                .newServiceRequestBuffer(ISCConstants.isc_action_svc_backup);
+    private ServiceRequestBuffer getBackupSRB(int options) {
 
-        backupSPB.addArgument(ISCConstants.isc_spb_dbname, getDatabase());
+        ServiceRequestBuffer backupSPB = createRequestBuffer(
+                ISCConstants.isc_action_svc_backup, 
+                options);
+
         backupSPB.addArgument(ISCConstants.isc_spb_bkp_file, getBackupPath());
         backupSPB.addArgument(ISCConstants.isc_spb_bkp_length, 2048);
 
@@ -140,80 +125,125 @@ public class FBBackupManager extends FBServiceManager implements BackupManager {
         return backupSPB;
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.firebirdsql.management.BackupManager#restoreDatabase(boolean)
+    /**
+     * @see org.firebirdsql.management.BackupManager#restoreDatabase()
      */
-    public void restoreDatabase(boolean verbose) throws SQLException,
-            IOException {
-        restoreDatabase(-1, -1, false, 0, verbose);
+    public void restoreDatabase() throws SQLException {
+        restoreDatabase(0);
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.firebirdsql.management.BackupManager#restoreDatabase(int, int,
-     *      boolean, int, boolean)
+    /**
+     * @see org.firebirdsql.management.BackupManager#restoreDatabase(int)
      */
-    public void restoreDatabase(int buffers, int pageSize,
-            boolean restoreReadOnly, int options, boolean verbose)
-            throws SQLException, IOException {
-        GDS gds = getGds();
+    public void restoreDatabase(int options) throws SQLException {
+        executeServicesOperation(getRestoreSRB(options));
+    }
 
-        try {
-            isc_svc_handle handle = attachServiceManager(gds);
 
-            try {
-                ServiceRequestBuffer restoreSRB = getRestoreSRB(buffers,
-                        pageSize, restoreReadOnly, options, verbose, gds);
-                gds.isc_service_start(handle, restoreSRB);
-
-                queueService(gds, handle);
-            } finally {
-                detachServiceManager(gds, handle);
-            }
-        } catch (GDSException ex) {
-            throw new FBSQLException(ex);
+    /**
+     * Set whether the operations of this <code>BackupManager</code> will
+     * result in verbose logging to the configured logger.
+     *
+     * @param verbose If <code>true</code>, operations will be logged
+     *        verbosely, otherwise they will not be logged verbosely
+     */
+    public void setVerbose(boolean verbose) {
+        this.verbose = verbose;
+    }
+      
+    /**
+     * Set the default number of pages to be buffered (cached) by default in a 
+     * restored database.
+     *
+     * @param bufferCount The page-buffer size to be used, a positive value
+     */
+    public void setRestorePageBufferCount(int bufferCount) {
+        if (bufferCount < 0){
+            throw new IllegalArgumentException("Buffer count must be positive");
         }
+        this.restoreBufferCount = bufferCount;
     }
+
+    /**
+     * Set the page size that will be used for a restored database. The value 
+     * for <code>pageSize</code> must be one of: 1024, 2048, 4196, or 8192. The
+     * default value is 1024.
+     *
+     * @param pageSize The page size to be used in a restored database,
+     *        one of 1024, 2048, 4196 or 8192
+     */
+    public void setRestorePageSize(int pageSize) {
+        if (pageSize != 1024 && pageSize != 2048 
+                && pageSize != 4196 && pageSize != 8192){
+            throw new IllegalArgumentException(
+                    "Page size must be one of 1024, 2048, 4196 or 8192");
+        }
+        this.restorePageSize = pageSize;
+    }
+
+    /**
+     * Set the restore operation to create a new database, as opposed to
+     * overwriting an existing database. This is true by default.
+     *
+     * @param create If <code>true</code>, the restore operation will attempt
+     *        to create a new database, otherwise the restore operation will
+     *        overwrite an existing database
+     */
+    public void setRestoreCreate(boolean create) {
+        this.restoreCreate = create;
+    }
+
+
+    /**
+     * Set the read-only attribute on a restored database.
+     *
+     * @param readOnly If <code>true</code>, a restored database will be
+     *        read-only, otherwise it will be read-write.
+     */
+    public void setRestoreReadOnly(boolean readOnly) {
+        this.restoreReadOnly = readOnly;
+    }
+
 
     /**
      * Creates and returns the "backup" service request buffer for the Service
      * Manager.
      * 
-     * @param buffers
-     * @param pageSize
-     * @param restoreReadOnly
-     * @param options
-     * @param verbose
-     * @param gds
+     * @param options THe options to be used for the backup operation
      * @return the "backup" service request buffer for the Service Manager.
      */
-    private ServiceRequestBuffer getRestoreSRB(int buffers, int pageSize,
-            boolean restoreReadOnly, int options, boolean verbose, GDS gds) {
+    private ServiceRequestBuffer getRestoreSRB(int options) {
+
+        GDS gds = getGds();
         ServiceRequestBuffer restoreSPB = gds
                 .newServiceRequestBuffer(ISCConstants.isc_action_svc_restore);
 
-        restoreSPB.addArgument(ISCConstants.isc_spb_bkp_file, getBackupPath());
-        restoreSPB.addArgument(ISCConstants.isc_spb_dbname, getDatabase());
+        // db-name and backup path are reversed for restore operations
+        restoreSPB.addArgument(ISCConstants.isc_spb_dbname, getBackupPath());
+        restoreSPB.addArgument(ISCConstants.isc_spb_bkp_file, getDatabase());
 
-        if (buffers != -1)
-            restoreSPB.addArgument(ISCConstants.isc_spb_res_buffers, buffers);
+        if (restoreBufferCount != -1)
+            restoreSPB.addArgument(ISCConstants.isc_spb_res_buffers, 
+                    restoreBufferCount);
 
-        if (pageSize != -1)
-            restoreSPB
-                    .addArgument(ISCConstants.isc_spb_res_page_size, pageSize);
+        if (restorePageSize != -1)
+            restoreSPB.addArgument(ISCConstants.isc_spb_res_page_size, 
+                    restorePageSize);
 
-        if (restoreReadOnly)
-            restoreSPB.addArgument(ISCConstants.isc_spb_res_access_mode,
-                    restoreReadOnly ? ISCConstants.isc_spb_res_am_readonly
-                            : ISCConstants.isc_spb_res_am_readwrite);
+        restoreSPB.addArgument(ISCConstants.isc_spb_res_access_mode,
+                    ((byte)(restoreReadOnly 
+                        ? ISCConstants.isc_spb_res_am_readonly
+                        : ISCConstants.isc_spb_res_am_readwrite)));
 
         if (verbose)
             restoreSPB.addArgument(ISCConstants.isc_spb_verbose);
 
-        restoreSPB.addArgument(ISCConstants.isc_spb_options, options | RESTORE_CREATE);
+        if ((options & RESTORE_CREATE) != RESTORE_CREATE 
+                && (options & RESTORE_REPLACE) != RESTORE_REPLACE){
+            options |= restoreCreate ? RESTORE_CREATE : RESTORE_REPLACE;
+        }
+        
+        restoreSPB.addArgument(ISCConstants.isc_spb_options, options);
 
         return restoreSPB;
     }
