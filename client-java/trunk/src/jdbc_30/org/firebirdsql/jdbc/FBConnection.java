37ee7f418c15e00506f1759321944bf47d9b1c17
rrokytskyy <rrokytskyy@65644016-39b1-43b1-bf79-96bc8fe82c15>
2005-04-24 15:31:16 +0000

    removed direct reference to managed connection

--- client-java/trunk/src/jdbc_30/org/firebirdsql/jdbc/FBConnection.java
+++ client-java/trunk/src/jdbc_30/org/firebirdsql/jdbc/FBConnection.java
@@ -233,7 +233,7 @@ public class FBConnection extends AbstractConnection {
         try {
             txCoordinator.ensureTransaction();
             
-            mc.getGDSHelper().executeImmediate("SAVEPOINT " + savepoint.getServerSavepointId());
+            getGDSHelper().executeImmediate("SAVEPOINT " + savepoint.getServerSavepointId());
         } catch(GDSException ex) {
             throw new FBSQLException(ex);
         }
@@ -288,7 +288,7 @@ public class FBConnection extends AbstractConnection {
             throw new SQLException("Savepoint is no longer valid.");
         
         try {
-            mc.getGDSHelper().executeImmediate(
+            getGDSHelper().executeImmediate(
                     "ROLLBACK TO " + fbSavepoint.getServerSavepointId());
         } catch (GDSException ex) {
             throw new FBSQLException(ex);
@@ -321,7 +321,7 @@ public class FBConnection extends AbstractConnection {
             throw new SQLException("Savepoint is no longer valid.");
 
         try {
-            mc.getGDSHelper().executeImmediate(
+            getGDSHelper().executeImmediate(
                     "RELEASE SAVEPOINT " + fbSavepoint.getServerSavepointId() + " ONLY");
         } catch (GDSException ex) {
             throw new FBSQLException(ex);
