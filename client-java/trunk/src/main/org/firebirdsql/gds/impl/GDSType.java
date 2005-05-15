cb7bad48e4cddb3c6c256c66530dc918481a0005
rrokytskyy <rrokytskyy@65644016-39b1-43b1-bf79-96bc8fe82c15>
2005-05-15 22:18:49 +0000

    removed commented code

--- client-java/trunk/src/main/org/firebirdsql/gds/impl/GDSType.java
+++ client-java/trunk/src/main/org/firebirdsql/gds/impl/GDSType.java
@@ -40,34 +40,6 @@ public final class GDSType implements Serializable {
     // needed to initiate static initialization of the GDSFactory
     private static final GDSFactory factory = new GDSFactory();
     
-//    public static final String PURE_JAVA_STR = "PURE_JAVA";
-//    public static final String TYPE4_STR = "TYPE4";
-    
-//    public static final String NATIVE_STR = "NATIVE";
-//    public static final String TYPE2_STR = "TYPE2";
-//    public static final String EMBEDDED_STR = "EMBEDDED";
-//    public static final String LOCAL_STR = "LOCAL";
-//    public static final String ORACLE_MODE_STR = "ORACLE_MODE";
-//    
-//    public static final GDSType PURE_JAVA = new GDSType(PURE_JAVA_STR);
-//    public static final GDSType NATIVE = new GDSType(NATIVE_STR);
-//    public static final GDSType NATIVE_EMBEDDED = new GDSType(EMBEDDED_STR);
-//    public static final GDSType NATIVE_LOCAL = new GDSType(LOCAL_STR);
-//    public static final GDSType ORACLE_MODE = new GDSType(ORACLE_MODE_STR);
-//
-//    private static final GDSType[] PRIVATE_VALUES = {
-//        PURE_JAVA, NATIVE, NATIVE_EMBEDDED, NATIVE_LOCAL, ORACLE_MODE
-//    };
-    
-//    static {
-//        STRING_MAP.put(PURE_JAVA_STR, PURE_JAVA);
-//        STRING_MAP.put(TYPE4_STR, PURE_JAVA);
-//        STRING_MAP.put(NATIVE_STR, NATIVE);
-//        STRING_MAP.put(TYPE2_STR, NATIVE);
-//        STRING_MAP.put(EMBEDDED_STR, NATIVE_EMBEDDED);
-//        STRING_MAP.put(LOCAL_STR, NATIVE_LOCAL);
-//        STRING_MAP.put(ORACLE_MODE_STR, ORACLE_MODE);
-//    }
     
     /**
      * Factory method for instances of this class. There's only three possible
