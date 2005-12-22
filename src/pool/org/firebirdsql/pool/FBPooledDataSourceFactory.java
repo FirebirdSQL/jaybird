4ee218a5fe5d9dd92d5c04b88df1379f168eb91e
sjardine <sjardine@users.sourceforge.net>
2005-12-22 02:02:41 +0000

    Added license and comments to new classes.

--- src/pool/org/firebirdsql/pool/FBPooledDataSourceFactory.java
+++ src/pool/org/firebirdsql/pool/FBPooledDataSourceFactory.java
@@ -1,13 +1,41 @@
+/*
+ * $Id$
+ * 
+ * Firebird Open Source J2EE Connector - JDBC Driver
+ *
+ * Distributable under LGPL license.
+ * You may obtain a copy of the License at http://www.gnu.org/copyleft/lgpl.html
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * LGPL License for more details.
+ *
+ * This file was created by members of the firebird development team.
+ * All individual contributions remain the Copyright (C) of those
+ * individuals.  Contributors to this file are either listed here or
+ * can be obtained from a CVS history command.
+ *
+ * All rights reserved.
+ */
 package org.firebirdsql.pool;
 
 import org.firebirdsql.gds.ClassFactory;
 import org.firebirdsql.logging.Logger;
 import org.firebirdsql.logging.LoggerFactory;
 
+/**
+ * Creates Pooled DataSource objects.
+ * 
+ * @author <a href="mailto:sjardine@users.sourceforge.net">Steven Jardine </a>
+ */
 public class FBPooledDataSourceFactory {
 
     private static Logger log = LoggerFactory.getLogger(FBPooledDataSourceFactory.class, false);
 
+    /**
+     * @return a new DriverConnectionPoolDatasource object.
+     */
     public static AbstractDriverConnectionPoolDataSource createDriverConnectionPoolDataSource() {
         try {
             return (AbstractDriverConnectionPoolDataSource) ClassFactory.get(
@@ -18,6 +46,9 @@ public class FBPooledDataSourceFactory {
         }
     }
 
+    /**
+     * @return a new FBConnectionPoolDataSource object.
+     */
     public static AbstractFBConnectionPoolDataSource createFBConnectionPoolDataSource() {
         try {
             return (AbstractFBConnectionPoolDataSource) ClassFactory.get(
