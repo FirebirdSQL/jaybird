29269c2626ad60f98104b3ff400c97fab06147e8
rrokytskyy <rrokytskyy@65644016-39b1-43b1-bf79-96bc8fe82c15>
2007-02-05 19:30:13 +0000

    added support for SQLstate mapping

--- client-java/trunk/src/main/org/firebirdsql/gds/GDSException.java
+++ client-java/trunk/src/main/org/firebirdsql/gds/GDSException.java
@@ -149,6 +149,19 @@ public class GDSException extends Exception {
         else
             return -1;
     }
+    
+    /**
+     * Get the SQL state of this exception.
+     * 
+     * @return the SQL state of this exception or <code>null</code> if this 
+     * object does not represent an error.
+     */
+    public String getSQLState() {
+        if (type == ISCConstants.isc_arg_number) 
+            return GDSExceptionHelper.getSQLState(intParam);
+        else
+            return null;
+    }
 
     /**
      * Get the <code>int</code> parameter for this exception.
