f0c5e11d470d6f6029336044cd54b2b726cc5c3e
rrokytskyy <rrokytskyy@65644016-39b1-43b1-bf79-96bc8fe82c15>
2008-12-05 14:35:09 +0000

    added possibility to specify test defaults in a property file

--- client-java/trunk/src/test/org/firebirdsql/common/SimpleFBTestBase.java
+++ client-java/trunk/src/test/org/firebirdsql/common/SimpleFBTestBase.java
@@ -18,66 +18,61 @@
  */
 package org.firebirdsql.common;
 
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+
 import junit.framework.TestCase;
 
 /**
- * Base class for test cases which can be run against only a single GDS implementation.
+ * Base class for test cases which can be run against only a single GDS
+ * implementation.
  */
-public class SimpleFBTestBase extends TestCase
-    {
-    /**
-     * Default name of database file to use for the test case.
-     */
-    protected final String DB_NAME = "fbtest.fdb";
+public class SimpleFBTestBase extends TestCase {
+	private static ResourceBundle testDefaults = ResourceBundle.getBundle("unit_test_defaults");
 
-    /**
-     * Default user name for database connection
-     */
-    protected final String DB_USER = "sysdba";
+	private static String getProperty(String property, String defaultValue) {
+		try {
+			return System.getProperty(property, testDefaults
+					.getString(property));
+		} catch (MissingResourceException ex) {
+			return System.getProperty(property, defaultValue);
+		}
+	}
 
-    /**
-     * Password for the default user for database connection
-     */
-    protected final String DB_PASSWORD = "masterkey";
+	/**
+	 * Default name of database file to use for the test case.
+	 */
+	protected final String DB_NAME = "fbtest.fdb";
 
-    /**
-     *
-     */
-    protected static final String DB_PATH        = System.getProperty("test.db.dir", "");
+	protected final String DB_USER = getProperty("test.user", "sysdba");
+	protected final String DB_PASSWORD = getProperty("test.password", "masterkey");
 
-    /**
-     *
-     */
-    protected static final String DB_SERVER_URL  = System.getProperty("test.db.host", "localhost");
+	protected static final String DB_PATH = getProperty("test.db.dir", "");
+	protected static final String DB_SERVER_URL = getProperty("test.db.host", "localhost");
+	protected static final int DB_SERVER_PORT = Integer.parseInt(getProperty("test.db.port", "3050"));
 
-    /**
-     *
-     */
-    protected static final int    DB_SERVER_PORT = Integer.parseInt(System.getProperty("test.db.port", "3050"));
+	/**
+	 * Builds an firebird database connection string for the supplied database
+	 * file.
+	 * 
+	 * @param name
+	 * @return
+	 */
+	protected String getdbpath(String name) {
+		if ("EMBEDDED".equalsIgnoreCase(getProperty("test.gds_type", null)))
+			return DB_PATH + "/" + name;
+		else if ("LOCAL".equalsIgnoreCase(getProperty("test.gds_type", null)))
+			return DB_PATH + "/" + name;
+		else
+			return DB_SERVER_URL + "/" + DB_SERVER_PORT + ":" + DB_PATH + "/"
+					+ name;
+	}
 
-    /**
-     * Builds an firebird database connection string for the supplied database file.
-     *
-     * @param name
-     * @return
-     */
-    protected String getdbpath(String name)
-       {
-       if ("EMBEDDED".equalsIgnoreCase(System.getProperty("test.gds_type")))
-           return DB_PATH + "/" + name;
-       else
-       if ("LOCAL".equalsIgnoreCase(System.getProperty("test.gds_type")))
-           return DB_PATH + "/" + name;
-       else
-           return DB_SERVER_URL + "/" + DB_SERVER_PORT + ":" + DB_PATH + "/" + name;
-       }
-
-    /**
-     *
-     * @param s
-     */
-    protected SimpleFBTestBase(String s)
-        {
-        super(s);
-        }
-    }
+	/**
+	 * 
+	 * @param s
+	 */
+	protected SimpleFBTestBase(String s) {
+		super(s);
+	}
+}
