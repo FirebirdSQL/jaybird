f9dc71b25e65a96026326225e44fbdd600a878e1
tsteinmaurer <tsteinmaurer@users.sourceforge.net>
2009-10-08 13:50:41 +0000

    Support for Trace/Audit via Services API in Firebird 2.5

---
+++ src/main/org/firebirdsql/management/FBTraceManager.java
@@ -0,0 +1,220 @@
+/*
+ * Firebird Open Source J2EE Connector - JDBC Driver
+ *
+ * Distributable under LGPL license.
+ * You may obtain a copy of the License at http://www.gnu.org/copyleft/lgpl.html
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * LGPL License for more details.
+ *
+ * This file was created by members of the firebird development team.
+ * All individual contributions remain the Copyright (C) of those
+ * individuals.  Contributors to this file are either listed here or
+ * can be obtained from a CVS history command.
+ *
+ * All rights reserved.
+ */
+package org.firebirdsql.management;
+
+import java.sql.SQLException;
+import java.io.*;
+
+import org.firebirdsql.gds.ISCConstants;
+import org.firebirdsql.gds.ServiceRequestBuffer;
+import org.firebirdsql.gds.impl.GDSType;
+import org.firebirdsql.jdbc.FBSQLException;
+
+/**
+ * Implements the Trace/Audit API available new in Firebird 2.5
+ * 
+ * @author <a href="mailto:tsteinmaurer@users.sourceforge.net">Thomas Steinmaurer</a>
+ */
+public class FBTraceManager extends FBServiceManager implements TraceManager {
+	
+    private class TraceStartThread extends Thread {
+    	
+    	private FBTraceManager ftm;
+    	private ServiceRequestBuffer srb;
+    	
+    	public TraceStartThread(FBTraceManager ftm, ServiceRequestBuffer srb) {
+    		this.ftm = ftm;
+    		this.srb = srb;
+    	}
+    	
+    	public void run() {
+    		try {
+    			System.out.println("Start trace");
+    			ftm.executeServicesOperation(srb);
+    			System.out.println("Trace stopped");
+    		} catch (FBSQLException e) {
+    			
+    		}
+    	}
+    }
+	
+	/**
+     * Create a new instance of <code>FBTraceManager</code> based on
+     * the default GDSType.
+     */
+    public FBTraceManager()
+    {
+    	super();
+    }
+
+    /**
+     * Create a new instance of <code>FBTraceManager</code> based on
+     * a given GDSType.
+     * 
+     * @param gdsType type must be PURE_JAVA, EMBEDDED, or NATIVE
+     */
+    public FBTraceManager(String gdsType)
+    {
+    	super(gdsType);
+    }
+
+    /**
+     * Create a new instance of <code>FBTraceManager</code> based on
+     * a given GDSType.
+     * 
+     * @param gdsType type must be PURE_JAVA, EMBEDDED, or NATIVE
+     */
+    public FBTraceManager(GDSType gdsType) {
+    	super(gdsType);
+    }
+    
+    /**
+     * Creates and returns the "trace" service request buffer for the Service
+     * Manager.
+     * 
+     * @param action The isc_action_svc_trace_* action to be used
+     * @return the "trace" service request buffer for the Service Manager.
+     * @throws SQLException
+     */
+	private ServiceRequestBuffer getTraceSPB(int action) throws SQLException {
+    	ServiceRequestBuffer traceSPB = getGds().createServiceRequestBuffer(action);
+        return traceSPB;
+    }
+
+	/**
+     * Creates and returns the "trace" service request buffer for the Service
+     * Manager.
+     * 
+	 * @param action The isc_action_svc_trace_* action to be used
+	 * @param traceSessionId The trace session ID
+	 * @return the "trace" service request buffer for the Service Manager.
+	 * @throws SQLException
+	 */
+	private ServiceRequestBuffer getTraceSPB(int action, int traceSessionId) throws SQLException {
+		ServiceRequestBuffer traceSPB = getTraceSPB(action);
+	    traceSPB.addArgument(ISCConstants.isc_spb_trc_id, traceSessionId);
+	    return traceSPB;
+	}
+
+	/**
+     * Creates and returns the "trace" service request buffer for the Service
+     * Manager.
+     * 
+	 * @param action The isc_action_svc_trace_* action to be used
+	 * @param traceSessionName The trace session name
+	 * @param configuration The trace configuration. For an example, look into fbtrace.conf in the root directory of your Firebird installation
+	 * @return the "trace" service request buffer for the Service Manager.
+	 * @throws SQLException
+	 */
+	private ServiceRequestBuffer getTraceSPB(int action, String traceSessionName, String configuration) throws SQLException {
+		ServiceRequestBuffer traceSPB = getTraceSPB(action);
+	    traceSPB.addArgument(ISCConstants.isc_spb_trc_name, traceSessionName);
+	    traceSPB.addArgument(ISCConstants.isc_spb_trc_cfg, configuration);
+	    return traceSPB;
+	}
+
+
+    /**
+     * Starts a trace session with an optioanl trace session name and configuration
+     *
+     * @param traceSessionName The trace session name (optional)
+     * @param configuration The trace configuration. For an example, look into fbtrace.conf in the root directory of your Firebird installation
+	 * @throws SQLException
+     */
+	public void startTraceSession(String traceSessionName, String configuration) throws SQLException {
+    	
+    	if ((configuration == null) || (configuration.equals(""))) {
+    		throw new FBSQLException("No configuration provided");
+    	}
+    	if (traceSessionName == null) {
+    		traceSessionName = "";
+    	}
+    	
+    	TraceStartThread t = new TraceStartThread(this, getTraceSPB(ISCConstants.isc_action_svc_trace_start, traceSessionName, configuration));
+    	t.start();
+    	
+    	//executeServicesOperation(getTraceSPB(ISCConstants.isc_action_svc_trace_start, traceSessionName, configuration));
+    	
+    }
+
+    /**
+     * Stops a trace session with the given trace session ID
+     *
+     * @param traceSessionId The trace session ID
+	 * @throws SQLException
+     */
+	public void stopTraceSession(int traceSessionId) throws SQLException {
+    	executeServicesOperation(getTraceSPB(ISCConstants.isc_action_svc_trace_stop, traceSessionId));
+    }
+    
+    /**
+     * Suspends a trace session with the given trace session ID
+     *
+     * @param traceSessionId The trace session ID
+	 * @throws SQLException
+     */
+    public void suspendTraceSession(int traceSessionId) throws SQLException {
+    	executeServicesOperation(getTraceSPB(ISCConstants.isc_action_svc_trace_suspend, traceSessionId));
+    }
+    
+    /**
+     * Resumes a trace session with the given trace session ID
+     *
+     * @param traceSessionId The trace session ID
+	 * @throws SQLException
+     */
+    public void resumeTraceSession(int traceSessionId) throws SQLException {
+    	executeServicesOperation(getTraceSPB(ISCConstants.isc_action_svc_trace_resume, traceSessionId));
+    }
+    
+    
+    /**
+     * List all currently registered trace sessions
+     * 
+	 * @throws SQLException
+     */
+	public void listTraceSessions() throws SQLException {
+		executeServicesOperation(getTraceSPB(ISCConstants.isc_action_svc_trace_list));
+	}
+	
+	/**
+	 * Loads a configuration from the specified fileName
+	 *
+	 * @throws FileNotFoundException
+	 * @throws IOException
+	 */
+	public String loadConfigurationFromFile(String fileName) throws FileNotFoundException, IOException {
+		
+		FileReader fr = new FileReader(fileName);
+		BufferedReader br = new BufferedReader(fr);
+		StringBuffer sb = new StringBuffer(256);
+		String s;
+		try {
+			while ((s = br.readLine()) != null) {
+				sb.append(s);
+			}
+		} finally {
+			br.close();
+			fr.close();
+		}
+		
+		return sb.toString();
+	}
+	
+}
---
+++ src/main/org/firebirdsql/management/TraceManager.java
@@ -0,0 +1,113 @@
+/*
+ * Firebird Open Source J2EE Connector - JDBC Driver
+ * 
+ * Copyright (C) All Rights Reserved.
+ * 
+ * This file was created by members of the firebird development team.
+ * All individual contributions remain the Copyright (C) of those
+ * individuals.  Contributors to this file are either listed here or
+ * can be obtained from a CVS history command.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *  
+ *   - Redistributions of source code must retain the above copyright 
+ *     notice, this list of conditions and the following disclaimer.
+ *   - Redistributions in binary form must reproduce the above 
+ *     copyright notice, this list of conditions and the following 
+ *     disclaimer in the documentation and/or other materials provided 
+ *     with the distribution.
+ *   - Neither the name of the firebird development team nor the names
+ *     of its contributors may be used to endorse or promote products 
+ *     derived from this software without specific prior written 
+ *     permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS 
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE 
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS 
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED 
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT 
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF 
+ * SUCH DAMAGE.
+ */
+
+package org.firebirdsql.management;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.SQLException;
+
+import org.firebirdsql.gds.ISCConstants;
+
+
+
+/**
+ * Implements the Trace/Audit API availabe new in Firebird 2.5. 
+ * This functionality includes:
+ * <ul>
+ *      <li>Starting a new trace session
+ *      <li>Stopping an existing trace session
+ *      <li>Suspending an existing trace session
+ *      <li>Resuming a suspended trace session
+ *      <li>Retrieving a list of trace sessions
+ * </ul>
+ *
+ * @author <a href="mailto:tsteinmaurer@users.sourceforge.net">Thomas Steinmaurer</a>
+ */
+public interface TraceManager extends ServiceManager {
+
+    /**
+     * Starts a trace session with an optioanl trace session name and configuration
+     *
+     * @param traceSessionName The trace session name (optional)
+     * @param configuration The trace configuration. For an example, look into fbtrace.conf in the root directory of your Firebird installation
+	 * @throws SQLException
+     */
+    public void startTraceSession(String traceSessionName, String configuration) throws SQLException;
+
+    /**
+     * Stops a trace session with the given trace session ID
+     *
+     * @param traceSessionId The trace session ID
+	 * @throws SQLException
+     */
+    public void stopTraceSession(int traceSessionId) throws SQLException;
+    
+    /**
+     * Suspends a trace session with the given trace session ID
+     *
+     * @param traceSessionId The trace session ID
+	 * @throws SQLException
+     */
+    public void suspendTraceSession(int traceSessionId) throws SQLException;
+    
+    /**
+     * Resumes a trace session with the given trace session ID
+     *
+     * @param traceSessionId The trace session ID
+	 * @throws SQLException
+     */
+    public void resumeTraceSession(int traceSessionId) throws SQLException;
+    
+    /**
+     * List all currently registered trace sessions
+     * 
+	 * @throws SQLException
+     */
+    public void listTraceSessions() throws SQLException;
+    
+	/**
+	 * Loads a configuration from the specified fileName
+	 *
+	 * @throws FileNotFoundException
+	 * @throws IOException
+	 */
+    public String loadConfigurationFromFile(String fileName) throws FileNotFoundException, IOException;
+    
+}
