192203963220c6a64a106fb3387c38b1523b57aa
mrotteveel <mrotteveel@65644016-39b1-43b1-bf79-96bc8fe82c15>
2011-10-27 21:15:44 +0000

    JDBC-192: Changes for generated keys to also work on Java 5

--- client-java/trunk/src/jdbc_30/org/firebirdsql/jdbc/FBPreparedStatement.java
+++ client-java/trunk/src/jdbc_30/org/firebirdsql/jdbc/FBPreparedStatement.java
@@ -90,16 +90,6 @@ public class FBPreparedStatement extends AbstractPreparedStatement {
     
     /**
      * jdbc 3
-     * @return <description>
-     * @exception java.sql.SQLException <description>
-     */
-    public ResultSet getGeneratedKeys() throws SQLException {
-        // TODO: implement this java.sql.Statement method
-        throw new SQLException("not yet implemented");
-    }
-    
-    /**
-     * jdbc 3
      * @param param1 <description>
      * @param param2 <description>
      * @return <description>
