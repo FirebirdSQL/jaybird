c7abc88702266ca69190695f26101267f12e8c72
mrotteveel <mrotteveel@65644016-39b1-43b1-bf79-96bc8fe82c15>
2011-12-04 12:58:15 +0000

    Modify test to use assertEquals instead of assertTrue with equals, and fail instead of assertTrue with false.

--- client-java/trunk/src/test/org/firebirdsql/jdbc/field/BaseTestFBField.java
+++ client-java/trunk/src/test/org/firebirdsql/jdbc/field/BaseTestFBField.java
@@ -1,4 +1,6 @@
- /*
+/*
+ * $Id$
+ * 
  * Firebird Open Source J2ee connector - jdbc driver
  *
  * Distributable under LGPL license.
@@ -16,7 +18,6 @@
  *
  * All rights reserved.
  */
-
 package org.firebirdsql.jdbc.field;
 
 import java.io.ByteArrayInputStream;
@@ -38,7 +39,7 @@ import org.firebirdsql.jdbc.FBResultSet;
 
 /**
  * Describe class <code>BaseTestFBField</code> here.
- *
+ * 
  * @author <a href="mailto:rrokytskyy@users.sourceforge.net">Roman Rokytskyy</a>
  * @version 1.0
  */
@@ -51,19 +52,18 @@ public abstract class BaseTestFBField extends TestCase {
     static float TEST_FLOAT = Float.MAX_VALUE;
     static double TEST_DOUBLE = Double.MAX_VALUE;
     static int TEST_BOOLEAN_INT = 1;
-    static byte[] TEST_BYTES = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    static byte[] TEST_BYTES = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
     static long TEST_TIME_LONG = System.currentTimeMillis();
 
     static Date TEST_DATE =
-        new Date(System.currentTimeMillis());
+    		new Date(System.currentTimeMillis());
 
     static Time TEST_TIME =
-        new Time(System.currentTimeMillis());
+    		new Time(System.currentTimeMillis());
 
     static Timestamp TEST_TIMESTAMP =
-        new Timestamp(System.currentTimeMillis());
-
+    		new Timestamp(System.currentTimeMillis());
 
     public BaseTestFBField(String testName) {
         super(testName);
@@ -86,51 +86,51 @@ public abstract class BaseTestFBField extends TestCase {
                 rs.row[0] = data;
             }
         };
-        
+
         return dataProvider;
     }
-    
+
     public void testByte() throws SQLException {
         field.setByte(TEST_BYTE);
-        field.copyOI();		  
-        assertTrue("Byte values test failure", field.getByte() == TEST_BYTE);
+        field.copyOI();
+        assertEquals("Byte values test failure", TEST_BYTE, field.getByte());
     }
 
     public void testShort() throws SQLException {
         field.setShort(TEST_SHORT);
-        field.copyOI();		  
-        assertTrue("Short values test failure", field.getShort() == TEST_SHORT);
+        field.copyOI();
+        assertEquals("Short values test failure", TEST_SHORT, field.getShort());
     }
 
     public void testInteger() throws SQLException {
         field.setInteger(TEST_INT);
-        field.copyOI();		  
-        assertTrue("Integer values test failure", field.getInt() == TEST_INT);
+        field.copyOI();
+        assertEquals("Integer values test failure", TEST_INT, field.getInt());
     }
 
     public void testLong() throws SQLException {
         field.setLong(TEST_LONG);
-        field.copyOI();		  
-        assertTrue("Long values test failure", field.getLong() == TEST_LONG);
+        field.copyOI();
+        assertEquals("Long values test failure", TEST_LONG, field.getLong());
     }
 
     public void testFloat() throws SQLException {
         field.setFloat(TEST_FLOAT);
-        field.copyOI();		  
-        assertTrue("Float values test failure", field.getFloat() == TEST_FLOAT);
+        field.copyOI();
+        assertEquals("Float values test failure", TEST_FLOAT, field.getFloat());
     }
 
     public void testDouble() throws SQLException {
         field.setDouble(TEST_DOUBLE);
-        field.copyOI();		  
-        assertTrue("Double values test failure", field.getDouble() == TEST_DOUBLE);
+        field.copyOI();
+        assertEquals("Double values test failure", TEST_DOUBLE, field.getDouble(), 0.0);
     }
 
     public abstract void testBigDecimal() throws SQLException;
 
     public void testBoolean() throws SQLException {
         field.setBoolean(true);
-        field.copyOI();		  
+        field.copyOI();
         assertTrue("Boolean values test failure", field.getBoolean());
     }
 
@@ -140,38 +140,44 @@ public abstract class BaseTestFBField extends TestCase {
 
     public void testAsciiStream() throws SQLException {
         field.setAsciiStream(new ByteArrayInputStream(TEST_BYTES), TEST_BYTES.length);
-        field.copyOI();		  
+        field.copyOI();
         assertTrue("ASCII stream values test failure", Arrays.equals(TEST_BYTES, readInputStream(field.getAsciiStream())));
     }
+
     public void testUnicodeStream() throws SQLException {
         field.setUnicodeStream(new ByteArrayInputStream(TEST_BYTES), TEST_BYTES.length);
-        field.copyOI();		  
+        field.copyOI();
         assertTrue("Unicode stream values test failure", Arrays.equals(TEST_BYTES, readInputStream(field.getUnicodeStream())));
     }
+
     public void testBinaryStream() throws SQLException {
         field.setBinaryStream(new ByteArrayInputStream(TEST_BYTES), TEST_BYTES.length);
-        field.copyOI();		  
+        field.copyOI();
         assertTrue("Binary stream values test failure", Arrays.equals(TEST_BYTES, readInputStream(field.getBinaryStream())));
     }
+
     public void testBytes() throws SQLException {
         field.setBytes(TEST_BYTES);
-        field.copyOI();		  
+        field.copyOI();
         assertTrue("Byte array values test failure", Arrays.equals(TEST_BYTES, field.getBytes()));
     }
+
     public void testDate() throws SQLException {
         field.setDate(TEST_DATE);
-        field.copyOI();		  
-        assertTrue("Date values test failure", field.getDate().toString().equals(TEST_DATE.toString()));
+        field.copyOI();
+        assertEquals("Date values test failure", TEST_DATE.toString(), field.getDate().toString());
     }
+
     public void testTime() throws SQLException {
         field.setTime(TEST_TIME);
-        field.copyOI();		  
-        assertTrue("Time values test failure", field.getTime().toString().equals(TEST_TIME.toString()));
+        field.copyOI();
+        assertEquals("Time values test failure", TEST_TIME.toString(), field.getTime().toString());
     }
+
     public void testTimestamp() throws SQLException {
         field.setTimestamp(TEST_TIMESTAMP);
-        field.copyOI();		  
-        assertTrue("Timestamp values test failure: expected: " + TEST_TIMESTAMP + ", actual: " + field.getTimestamp(), field.getTimestamp().equals(TEST_TIMESTAMP));
+        field.copyOI();
+        assertEquals("Timestamp values test failure", TEST_TIMESTAMP, field.getTimestamp());
     }
 
     protected byte[] readInputStream(InputStream in) {
@@ -182,31 +188,24 @@ public abstract class BaseTestFBField extends TestCase {
             while ((counter = in.read(buff)) != -1)
                 out.write(buff, 0, counter);
             return out.toByteArray();
-        } catch(IOException ioex) {
+        } catch (IOException ioex) {
             assertTrue("IOException happened during processing.", false);
             // lets make compiler happy! :)
             return null;
         }
     }
 
-    protected XSQLVAR createXSQLVAR()
-        {
-        if(getGdsType() == GDSType.getType("PURE_JAVA"))
+    protected XSQLVAR createXSQLVAR() {
+        if (getGdsType() == GDSType.getType("PURE_JAVA"))
             return new org.firebirdsql.gds.XSQLVAR();
-        else
-        if(getGdsType() == GDSType.getType("NATIVE") ||
-           getGdsType() == GDSType.getType("EMBEDDED"))
+        else if (getGdsType() == GDSType.getType("NATIVE")
+                || getGdsType() == GDSType.getType("EMBEDDED"))
             return new XSQLVARLittleEndianImpl();
         else
             throw new RuntimeException("Unrecognised GDSType");
-        }
+    }
 
-
-    private GDSType getGdsType()
-        {
+    protected GDSType getGdsType() {
         return GDSType.getType("PURE_JAVA");
-        }
-
-
-
+    }
 }
--- client-java/trunk/src/test/org/firebirdsql/jdbc/field/TestFBIntegerField.java
+++ client-java/trunk/src/test/org/firebirdsql/jdbc/field/TestFBIntegerField.java
@@ -30,125 +30,135 @@ import junit.framework.TestSuite;
 
 /**
  * Describe class <code>TestFBIntegerField</code> here.
- *
+ * 
  * @author <a href="mailto:rrokytskyy@users.sourceforge.net">Roman Rokytskyy</a>
  * @version 1.0
  */
 public class TestFBIntegerField extends BaseTestFBField {
-	public TestFBIntegerField(String testName) {
-		super(testName);
-	}
-	public static Test suite() {
-		return new TestSuite(TestFBIntegerField.class);
-	}
-	protected void setUp() throws SQLException{
+    
+    public TestFBIntegerField(String testName) {
+        super(testName);
+    }
+
+    public static Test suite() {
+        return new TestSuite(TestFBIntegerField.class);
+    }
+
+    protected void setUp() throws SQLException {
         XSQLVAR[] xsqlvars = new XSQLVAR[1];
         xsqlvars[0] = createXSQLVAR();
         xsqlvars[0].sqltype = ISCConstants.SQL_LONG;
         field = FBField.createField(xsqlvars[0], createDataProvider(xsqlvars), null, false);
-	}
-	protected void tearDown() {
-	}
+    }
 
-	public void testObject() throws SQLException {
-		field.setObject(new Integer(TEST_INT));
-		field.copyOI();
-		assertTrue(field.getObject().equals(new Integer(TEST_INT)));
-	}
-	public void testUnicodeStream() throws SQLException {
-		try {
-			super.testUnicodeStream();
-			assertTrue("This method should fail.", false);
-		} catch(SQLException ex) {
-			//everything is ok :)
-		}
-	}
-	public void testByte() throws SQLException {
-		super.testByte();
-	}
-	public void testBinaryStream() throws SQLException {
-		try {
-			super.testBinaryStream();
-			assertTrue("This method should fail.", false);
-		} catch(SQLException ex) {
-			//everything is ok :)
-		}
+    public void testObject() throws SQLException {
+        field.setObject(new Integer(TEST_INT));
+        field.copyOI();
+        assertEquals(Integer.valueOf(TEST_INT), field.getObject());
+    }
 
-	}
-	public void testString() throws SQLException {
-		field.setString(Integer.toString(TEST_INT));
-		field.copyOI();
-		assertTrue(field.getString().equals(Integer.toString(TEST_INT)));
-	}
-	public void testAsciiStream() throws SQLException {
-		try {
-			super.testAsciiStream();
-			assertTrue("This method should fail.", false);
-		} catch(SQLException ex) {
-			//everything is ok :)
-		}
-	}
-	public void testTimestamp() throws SQLException {
-		try {
-			super.testTimestamp();
-			assertTrue("This method should fail.", false);
-		} catch(SQLException ex) {
-			//everything is ok :)
-		}
-	}
-	public void testBigDecimal() throws SQLException {
-		BigDecimal testBigDecimal =	BigDecimal.valueOf((long)TEST_INT);
-		field.setBigDecimal(testBigDecimal);
-		field.copyOI();
-		assertTrue(field.getBigDecimal().equals(testBigDecimal));
-	}
-	public void testDate() throws SQLException {
-		try {
-			super.testDate();
-			assertTrue("This method should fail.", false);
-		} catch(SQLException ex) {
-			//everything is ok :)
-		}
-	}
-	public void testTime() throws SQLException {
-		try {
-			super.testTime();
-			assertTrue("This method should fail.", false);
-		} catch(SQLException ex) {
-			//everything is ok :)
-		}
-	}
-	public void testBytes() throws SQLException {
-		try {
-			super.testBytes();
-			assertTrue("This method should fail.", false);
-		} catch(SQLException ex) {
-			//everything is ok :)
-		}
+    public void testUnicodeStream() throws SQLException {
+        try {
+            super.testUnicodeStream();
+            fail("This method should fail.");
+        } catch (SQLException ex) {
+            // everything is ok :)
+        }
+    }
 
-	}
-	public void testLong() throws SQLException {
-		try {
-			super.testLong();
-			assertTrue("This method should fail.", false);
-		} catch(SQLException ex) {
-			//everything is ok :)
-		}
-	}
-	public void testFloat() throws SQLException {
-		try {
-			super.testFloat();
-			assertTrue("This method should fail.", false);
-		} catch(SQLException sqlex) {
-			//everything is ok :)
-		}
-	}
-	public void testDouble() throws SQLException {
-		try {
-			super.testDouble();
-			assertTrue("This method should fail.", false);
-		} catch(SQLException sqlex) {
-			//everything is ok :)
-		}
-	}
+    public void testBinaryStream() throws SQLException {
+        try {
+            super.testBinaryStream();
+            fail("This method should fail.");
+        } catch (SQLException ex) {
+            // everything is ok :)
+        }
+
+    }
+
+    public void testString() throws SQLException {
+        field.setString(Integer.toString(TEST_INT));
+        field.copyOI();
+        assertEquals(Integer.toString(TEST_INT), field.getString());
+    }
+
+    public void testAsciiStream() throws SQLException {
+        try {
+            super.testAsciiStream();
+            fail("This method should fail.");
+        } catch (SQLException ex) {
+            // everything is ok :)
+        }
+    }
+
+    public void testTimestamp() throws SQLException {
+        try {
+            super.testTimestamp();
+            fail("This method should fail.");
+        } catch (SQLException ex) {
+            // everything is ok :)
+        }
+    }
+
+    public void testBigDecimal() throws SQLException {
+        BigDecimal testBigDecimal = BigDecimal.valueOf(TEST_INT);
+        field.setBigDecimal(testBigDecimal);
+        field.copyOI();
+        assertEquals(testBigDecimal, field.getBigDecimal());
+    }
+
+    public void testDate() throws SQLException {
+        try {
+            super.testDate();
+            fail("This method should fail.");
+        } catch (SQLException ex) {
+            // everything is ok :)
+        }
+    }
+
+    public void testTime() throws SQLException {
+        try {
+            super.testTime();
+            fail("This method should fail.");
+        } catch (SQLException ex) {
+            // everything is ok :)
+        }
+    }
+
+    public void testBytes() throws SQLException {
+        try {
+            super.testBytes();
+            fail("This method should fail.");
+        } catch (SQLException ex) {
+            // everything is ok :)
+        }
+
+    }
+
+    public void testLong() throws SQLException {
+        try {
+            super.testLong();
+            fail("This method should fail.");
+        } catch (SQLException ex) {
+            // everything is ok :)
+        }
+    }
+
+    public void testFloat() throws SQLException {
+        try {
+            super.testFloat();
+            fail("This method should fail.");
+        } catch (SQLException sqlex) {
+            // everything is ok :)
+        }
+    }
+
+    public void testDouble() throws SQLException {
+        try {
+            super.testDouble();
+            fail("This method should fail.");
+        } catch (SQLException sqlex) {
+            // everything is ok :)
+        }
+    }
 }
