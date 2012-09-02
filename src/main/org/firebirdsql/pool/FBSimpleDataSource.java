fca43320ee13a7e923f18be1afe7ec8f2b703964
mrotteveel <mrotteveel@users.sourceforge.net>
2012-09-02 11:11:30 +0000

    Move main implementation of FBSimpleDataSource to org.firebirdsql.ds and add deprecated subclass version in org.firebirdsql.pool for backward compatibility

---
+++ src/main/org/firebirdsql/pool/FBSimpleDataSource.java
@@ -0,0 +1,25 @@
+package org.firebirdsql.pool;
+
+import org.firebirdsql.gds.impl.GDSType;
+
+/**
+ * @deprecated Use {@link org.firebirdsql.dl.FBSimpleDataSource}
+ */
+public class FBSimpleDataSource extends org.firebirdsql.ds.FBSimpleDataSource {
+    
+    private static final long serialVersionUID = 3156578540634970427L;
+
+    /**
+     * Create instance of this class.
+     */
+    public FBSimpleDataSource() {
+        super();
+    }
+
+    /**
+     * Create instance of this class.
+     */
+    public FBSimpleDataSource(GDSType type) {
+        super(type);
+    }
+}
