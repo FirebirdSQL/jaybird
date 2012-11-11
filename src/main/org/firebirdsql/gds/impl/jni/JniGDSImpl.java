037090c791e0b9a83ba70d8e63e7a664c5651505
mrotteveel <mrotteveel@users.sourceforge.net>
2012-11-11 12:49:00 +0000

    * Change Jaybird library version to 2.3 * Make Visual Studio config use common settings for naming libs, instead of assigning it again and again for every individual build configuration

--- src/main/org/firebirdsql/gds/impl/jni/JniGDSImpl.java
+++ src/main/org/firebirdsql/gds/impl/jni/JniGDSImpl.java
@@ -1,3 +1,23 @@
+/*
+ * $Id$
+ * 
+ * Firebird Open Source J2ee connector - jdbc driver
+ *
+ * Distributable under LGPL license.
+ * You may obtain a copy of the License at http://www.gnu.org/copyleft/lgpl.html
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * LGPL License for more details.
+ *
+ * This file was created by members of the firebird development team.
+ * All individual contributions remain the Copyright (C) of those
+ * individuals.  Contributors to this file are either listed here or
+ * can be obtained from a CVS history command.
+ *
+ * All rights reserved.
+ */
 package org.firebirdsql.gds.impl.jni;
 
 import java.security.AccessController;
@@ -8,7 +28,6 @@ import org.firebirdsql.gds.impl.GDSType;
 import org.firebirdsql.logging.Logger;
 import org.firebirdsql.logging.LoggerFactory;
 
-
 public abstract class JniGDSImpl extends BaseGDSImpl {
 
     private static final boolean DEVELOPMENT_DEBUG_OUTPUT = true;
@@ -17,8 +36,8 @@ public abstract class JniGDSImpl extends BaseGDSImpl {
      * Name of the JNI library able to communicate with the client or embedded
      * server library.
      */
-    public static final String JAYBIRD_JNI_LIBRARY = "jaybird22";
-    public static final String JAYBIRD_JNI_LIBRARY_X64 = "jaybird22_x64";
+    public static final String JAYBIRD_JNI_LIBRARY = "jaybird23";
+    public static final String JAYBIRD_JNI_LIBRARY_X64 = "jaybird23_x64";
     
     private static Logger log = LoggerFactory.getLogger(JniGDSImpl.class,
             false);
