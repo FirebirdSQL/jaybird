e0b2203416cf944b927bfa4fcbf142da83d0fce3
mrotteveel <mrotteveel@users.sourceforge.net>
2012-11-17 13:30:22 +0000

    JDBC-266 On closer examination isc_spb_tra_id is an information item as part of isc_spb_multi_tra_id and not actually a limbo transaction

--- src/main/org/firebirdsql/management/FBMaintenanceManager.java
+++ src/main/org/firebirdsql/management/FBMaintenanceManager.java
@@ -286,7 +286,6 @@ public class FBMaintenanceManager extends FBServiceManager implements Maintenanc
             int idx = 0;
             while (idx < output.length) {
                 switch (output[idx++]) {
-                case ISCConstants.isc_spb_tra_id:
                 case ISCConstants.isc_spb_single_tra_id:
                 case ISCConstants.isc_spb_multi_tra_id:
                     int trId = getGds().iscVaxInteger(output, idx, 4);
@@ -294,6 +293,9 @@ public class FBMaintenanceManager extends FBServiceManager implements Maintenanc
                     result.add(Integer.valueOf(trId));
                     break;
                 // Information items we will ignore for now
+                case ISCConstants.isc_spb_tra_id:
+                    idx += 4;
+                    break;
                 case ISCConstants.isc_spb_tra_state:
                 case ISCConstants.isc_spb_tra_advise:
                     idx++;
