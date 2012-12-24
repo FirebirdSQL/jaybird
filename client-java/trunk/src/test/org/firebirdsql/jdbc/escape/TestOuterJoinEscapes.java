665b08a9ad7b33b21d3c7842d3e845272d9dd355
mrotteveel <mrotteveel@65644016-39b1-43b1-bf79-96bc8fe82c15>
2012-12-24 12:04:01 +0000

    Fix formatting

--- client-java/trunk/src/test/org/firebirdsql/jdbc/escape/TestOuterJoinEscapes.java
+++ client-java/trunk/src/test/org/firebirdsql/jdbc/escape/TestOuterJoinEscapes.java
@@ -49,9 +49,17 @@ public class TestOuterJoinEscapes extends FBJUnit4TestBase {
         Connection con = FBTestProperties.getConnectionViaDriverManager();
         try {
             Statement stmt = con.createStatement();
-            stmt.execute("CREATE TABLE TAB1 (" + "  ID INT CONSTRAINT PK_TAB1 PRIMARY KEY" + ")");
-            stmt.execute("CREATE TABLE TAB2 (" + "  ID INT CONSTRAINT PK_TAB2 PRIMARY KEY,"
-                    + "  TAB1_ID INT CONSTRAINT FK_TAB2_TAB1 REFERENCES TAB1 (ID)" + ")");
+//@formatter:off
+            stmt.execute(
+                    "CREATE TABLE TAB1 (" +
+                    "  ID INT CONSTRAINT PK_TAB1 PRIMARY KEY" +
+                    ")");
+            stmt.execute(
+                    "CREATE TABLE TAB2 (" +
+                    "  ID INT CONSTRAINT PK_TAB2 PRIMARY KEY," +
+                    "  TAB1_ID INT CONSTRAINT FK_TAB2_TAB1 REFERENCES TAB1 (ID)" +
+                    ")");
+//@formatter:on
             stmt.close();
             con.setAutoCommit(false);
             PreparedStatement pstmt = con.prepareStatement("INSERT INTO TAB1 (ID) VALUES (?)");
@@ -81,8 +89,12 @@ public class TestOuterJoinEscapes extends FBJUnit4TestBase {
         Connection con = FBTestProperties.getConnectionViaDriverManager();
         try {
             Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT TAB1.ID, TAB2.ID "
-                    + "FROM {oj TAB1 FULL OUTER JOIN TAB2 ON TAB2.TAB1_ID = TAB1.ID} " + "ORDER BY TAB1.ID NULLS LAST");
+//@formatter:off
+            ResultSet rs = stmt.executeQuery(
+                    "SELECT TAB1.ID, TAB2.ID " +
+                    "FROM {oj TAB1 FULL OUTER JOIN TAB2 ON TAB2.TAB1_ID = TAB1.ID} " +
+                    "ORDER BY TAB1.ID NULLS LAST");
+//@formatter:on
 
             assertTrue("Expected first row", rs.next());
             assertEquals(1, rs.getInt(1));
@@ -109,8 +121,12 @@ public class TestOuterJoinEscapes extends FBJUnit4TestBase {
         Connection con = FBTestProperties.getConnectionViaDriverManager();
         try {
             Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT TAB1.ID, TAB2.ID "
-                    + "FROM {oj TAB1 LEFT OUTER JOIN TAB2 ON TAB2.TAB1_ID = TAB1.ID} " + "ORDER BY TAB1.ID NULLS LAST");
+//@formatter:off
+            ResultSet rs = stmt.executeQuery(
+                    "SELECT TAB1.ID, TAB2.ID " +
+                    "FROM {oj TAB1 LEFT OUTER JOIN TAB2 ON TAB2.TAB1_ID = TAB1.ID} " +
+                    "ORDER BY TAB1.ID NULLS LAST");
+//@formatter:on
 
             assertTrue("Expected first row", rs.next());
             assertEquals(1, rs.getInt(1));
@@ -133,10 +149,12 @@ public class TestOuterJoinEscapes extends FBJUnit4TestBase {
         Connection con = FBTestProperties.getConnectionViaDriverManager();
         try {
             Statement stmt = con.createStatement();
-            ResultSet rs = stmt
-                    .executeQuery("SELECT TAB1.ID, TAB2.ID "
-                            + "FROM {oj TAB1 RIGHT OUTER JOIN TAB2 ON TAB2.TAB1_ID = TAB1.ID} "
-                            + "ORDER BY TAB1.ID NULLS LAST");
+//@formatter:off
+            ResultSet rs = stmt.executeQuery(
+                    "SELECT TAB1.ID, TAB2.ID " +
+                    "FROM {oj TAB1 RIGHT OUTER JOIN TAB2 ON TAB2.TAB1_ID = TAB1.ID} " +
+                    "ORDER BY TAB1.ID NULLS LAST");
+//@formatter:on
 
             assertTrue("Expected first row", rs.next());
             assertEquals(1, rs.getInt(1));
