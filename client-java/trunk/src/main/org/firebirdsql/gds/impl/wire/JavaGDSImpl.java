58d41cd0b64e5178176f42c789b9022678724d06
mrotteveel <mrotteveel@65644016-39b1-43b1-bf79-96bc8fe82c15>
2013-02-10 11:19:27 +0000

    * Move common methods to super-class * Change license of AbstractGDS to LGPL as code moved in is under that license (and rest was boilerplate) *Use separate iscVaxInteger implementation for 2-byte integers (rudimentary testing shows it performs a little bit better) * Add iscVaxLong (which is equivalent to iscVaxInteger, but then suitable for decoding 8-byte longs)

--- client-java/trunk/src/main/org/firebirdsql/gds/impl/wire/JavaGDSImpl.java
+++ client-java/trunk/src/main/org/firebirdsql/gds/impl/wire/JavaGDSImpl.java
@@ -1,14 +1,7 @@
 package org.firebirdsql.gds.impl.wire;
 
 /**
- * Implements JDK1.4+ specific socket creation.
- * 
- * This is necessary because of a bug found in the JDK1.4 and is / will not be
- * fixed until JDK7.0.
- * 
- * See bug details: <a
- * href='http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=5092063'>
- * http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=5092063 </a>
+ * TODO: Merge with AbstractJavaGDSImpl
  * 
  * @author <a href="mailto:sjardine@users.sourceforge.net">Steve Jardine </a>
  */
