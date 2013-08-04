58e4ff213a2572b3f0672a2782e5a39b98cba4a9
mrotteveel <mrotteveel@users.sourceforge.net>
2013-08-04 10:54:08 +0000

    Fix test for Firebird 3.0

--- src/test/org/firebirdsql/management/TestUserManager.java
+++ src/test/org/firebirdsql/management/TestUserManager.java
@@ -20,11 +20,19 @@
  */
 package org.firebirdsql.management;
 
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
 import java.sql.SQLException;
 
-import org.firebirdsql.common.FBTestBase;
+import org.firebirdsql.common.FBJUnit4TestBase;
+import org.firebirdsql.common.JdbcResourceHelper;
+import org.junit.Ignore;
+import org.junit.Test;
 
 import static org.firebirdsql.common.FBTestProperties.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
 /**
  * Tests the UserManager class which uses the Services API to display, add,
@@ -32,33 +40,19 @@ import static org.firebirdsql.common.FBTestProperties.*;
  * 
  * @author <a href="mailto:sjardine@users.sourceforge.net">Steven Jardine </a>
  */
-public class TestUserManager extends FBTestBase {
+public class TestUserManager extends FBJUnit4TestBase {
 
-//    private FBManager fbManager;
-
-    /**
-     * Create and instance of this class.
-     */
-    public TestUserManager(String name) {
-        super(name);
-    }
-
-    protected void setUp() throws Exception {
-        super.setUp();
-
-//        fbManager = createFBManager();
-//        fbManager.setDropOnStop(false);
-//        fbManager.setServer(DB_SERVER_URL);
-//        fbManager.setPort(DB_SERVER_PORT);
-//        fbManager.start();
-    }
-
-    protected void tearDown() throws Exception {
-//        fbManager.stop();
-        super.tearDown();
-    }
-
+    @Test
     public void testUsers() throws Exception {
+        boolean isFirebird3 = false;
+        Connection connection = null;
+        try {
+            connection = getConnectionViaDriverManager();
+            DatabaseMetaData dbmd = connection.getMetaData();
+            isFirebird3 = dbmd.getDatabaseMajorVersion() == 3;
+        } finally {
+            JdbcResourceHelper.closeQuietly(connection);
+        }
 
         // Initialize the UserManager.
         UserManager userManager = new FBUserManager(getGdsType());
@@ -74,44 +68,44 @@ public class TestUserManager extends FBTestBase {
         user1.setFirstName("First Name");
         user1.setMiddleName("Middle Name");
         user1.setLastName("Last Name");
-        user1.setUserId(222);
-        user1.setGroupId(222);
+        // Setting userid and groupid to 0 for Firebird 3 as it isn't supported for the SRP usermanager
+        user1.setUserId(isFirebird3 ? 0 : 222);
+        user1.setGroupId(isFirebird3 ? 0 : 222);
 
         try {
             userManager.add(user1);
         } catch(SQLException ex) {
-            // 
+            // ignore
         }
         
         // Check to make sure the user was added.
         User user2 = userManager.getUsers().get(user1.getUserName());
 
-        // User2 should not be null;
-        assertTrue("User 2 should not be null.", user2 != null);
-
-        assertTrue("user1 should equal user2", user1.equals(user2));
+        assertNotNull("User 2 should not be null.", user2);
+        assertEquals("user1 should equal user2", user1, user2);
 
         user1.setPassword("123test");
         user1.setFirstName("Name First");
         user1.setMiddleName("Name Middle");
         user1.setLastName("Name Last");
-        user1.setUserId(111);
-        user1.setGroupId(111);
+        user1.setUserId(isFirebird3 ? 0 : 111);
+        user1.setGroupId(isFirebird3 ? 0 : 111);
 
         userManager.update(user1);
 
         user2 = userManager.getUsers().get(user1.getUserName());
-        assertTrue("user1 should equal user2", user1.equals(user2));
+
+        assertEquals("user1 should equal user2", user1, user2);
 
         userManager.delete(user1);
 
         user2 = userManager.getUsers().get(user1.getUserName());
 
-        // User2 should be null;
-        assertTrue("User 2 should be null", user2 == null);
-
+        assertNull("User 2 should be null", user2);
     }
 
+    @Ignore
+    @Test
     public void testConnection() throws Exception {
 
         // TODO: Test use of user with database connection and sql.
