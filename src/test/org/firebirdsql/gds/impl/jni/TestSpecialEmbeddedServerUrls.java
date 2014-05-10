8699401cdc39c422b11f7e9d189a7515c011cefb
mrotteveel <mrotteveel@users.sourceforge.net>
2014-05-10 11:03:02 +0000

    Convert to JUnit 4, ignore for types other than embedded, code cleanup

--- src/test/org/firebirdsql/gds/impl/jni/TestSpecialEmbeddedServerUrls.java
+++ src/test/org/firebirdsql/gds/impl/jni/TestSpecialEmbeddedServerUrls.java
@@ -1,108 +1,107 @@
+/*
+ * $Id$
+ *
+ * Firebird Open Source JavaEE Connector - JDBC Driver
+ *
+ * Distributable under LGPL license.
+ * You may obtain a copy of the License at http://www.gnu.org/copyleft/lgpl.html
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * LGPL License for more details.
+ *
+ * This file was created by members of the firebird development team.
+ * All individual contributions remain the Copyright (C) of those
+ * individuals.  Contributors to this file are either listed here or
+ * can be obtained from a source control history command.
+ *
+ * All rights reserved.
+ */
 package org.firebirdsql.gds.impl.jni;
 
-import junit.framework.TestCase;
+import org.firebirdsql.common.rules.TestTypeRule;
 import org.firebirdsql.management.FBManager;
 import org.firebirdsql.gds.impl.GDSType;
-import org.firebirdsql.logging.Logger;
-import org.firebirdsql.logging.LoggerFactory;
 import org.firebirdsql.jdbc.FBDriver;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
 
 import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
 /**
- * Created by IntelliJ IDEA.
- * User: Ryan Baldwin
- * Date: 15-Oct-2003
- * Time: 17:33:53
- * To change this template use Options | File Templates.
+ * @author Ryan Baldwin
  */
-public class TestSpecialEmbeddedServerUrls extends TestCase
-    {
-     protected final Logger log = LoggerFactory.getLogger(getClass(),true);
+public class TestSpecialEmbeddedServerUrls {
 
+    @Rule
+    public final TestTypeRule testType = TestTypeRule.supports(EmbeddedGDSImpl.EMBEDDED_TYPE_NAME);
 
-    public TestSpecialEmbeddedServerUrls(String s)
-        {
-        super(s);
-        }
+    private String mRelativeDatabasePath;
+    private String mAbsoluteDatabasePath;
+    private FBManager fbManager;
+    private GDSType gdsType;
 
-    protected void setUp() throws Exception
-        {
-        try
-            {
-            Class.forName(FBDriver.class.getName());
+    @Before
+    public void setUp() throws Exception {
+        Class.forName(FBDriver.class.getName());
+        gdsType = GDSType.getType(EmbeddedGDSImpl.EMBEDDED_TYPE_NAME);
+        fbManager = new FBManager(gdsType);
 
-            fbManager = new FBManager(GDSType.getType("EMBEDDED"));
+        fbManager.setServer("localhost");
+        fbManager.setPort(5066);
+        fbManager.start();
 
-            fbManager.setServer("localhost");
-            fbManager.setPort(5066);
-            fbManager.start();
+        mRelativeDatabasePath = "db/testES01874.fdb";
+        mAbsoluteDatabasePath = new File(".", mRelativeDatabasePath).getAbsolutePath();
 
-            mRelativeDatabasePath = "db/" + "testES01874.fdb";
-            mAbsoluteDatabasePath = new File(".", mRelativeDatabasePath).getAbsolutePath();
+        fbManager.createDatabase(mAbsoluteDatabasePath, "SYSDBA", "masterkey");
+    }
 
-            fbManager.createDatabase(mAbsoluteDatabasePath, "SYSDBA", "masterkey");
-            }
-        catch (Exception e)
-            {
-            if (log!=null) log.warn("exception in setup of " + getName() + ": ", e);
-            } // end of try-catch
-        }
+    @After
+    public void tearDown() throws Exception {
+        fbManager.dropDatabase(mAbsoluteDatabasePath, "SYSDBA", "masterkey");
+        fbManager.stop();
+        fbManager = null;
+    }
 
-    protected void tearDown() throws Exception
-        {
-        try
-            {
-            fbManager.dropDatabase(mAbsoluteDatabasePath, "SYSDBA", "masterkey");
-            fbManager.stop();
-            fbManager = null;
-            }
-        catch (Exception e)
-            {
-            if (log!=null) log.warn("exception in teardown of " + getName() + ": ", e);
-            } // end of try-catch
-        }
-
-    public void testFBManagerWithoutSettingServerAndPort() throws Exception
-        {
-        FBManager testFBManager = new FBManager(GDSType.getType("EMBEDDED"));
+    @Test
+    public void testFBManagerWithoutSettingServerAndPort() throws Exception {
+        FBManager testFBManager = new FBManager(gdsType);
         testFBManager.start();
 
         testFBManager.dropDatabase(mAbsoluteDatabasePath, "SYSDBA", "masterkey");
         testFBManager.createDatabase(mAbsoluteDatabasePath, "SYSDBA", "masterkey");
 
         testFBManager.stop();
-        }
+    }
 
-    public void testFBManagerWithRelativeDatabaseFile() throws Exception
-        {
-        FBManager testFBManager = new FBManager(GDSType.getType("EMBEDDED"));
+    @Test
+    public void testFBManagerWithRelativeDatabaseFile() throws Exception {
+        FBManager testFBManager = new FBManager(gdsType);
         testFBManager.start();
 
         testFBManager.dropDatabase(mRelativeDatabasePath, "SYSDBA", "masterkey");
         testFBManager.createDatabase(mRelativeDatabasePath, "SYSDBA", "masterkey");
 
         testFBManager.stop();
-        }
-
-    public void testDriverManagerGetConnectionWithoutServerAndPortInUrl() throws Exception
-        {
-        Connection connection = DriverManager.getConnection("jdbc:firebirdsql:embedded:"+mAbsoluteDatabasePath, "SYSDBA", "masterkey");
-        connection.close();
-        }
-
-    public void testDriverManagerGetConnectionWithoutServerAndPortInUrlWithRelativeDatabasePath() throws Exception
-        {
-        Connection connection = DriverManager.getConnection("jdbc:firebirdsql:embedded:"+mRelativeDatabasePath, "SYSDBA", "masterkey");
-        connection.close();
-        }
-
-
-
-    private String mRelativeDatabasePath = null;
-    private String mAbsoluteDatabasePath = null;
-
-    private FBManager fbManager = null;
     }
+
+    @Test
+    public void testDriverManagerGetConnectionWithoutServerAndPortInUrl() throws Exception {
+        Connection connection = DriverManager.getConnection("jdbc:firebirdsql:embedded:" + mAbsoluteDatabasePath,
+                "SYSDBA", "masterkey");
+        connection.close();
+    }
+
+    @Test
+    public void testDriverManagerGetConnectionWithoutServerAndPortInUrlWithRelativeDatabasePath() throws Exception {
+        Connection connection = DriverManager.getConnection("jdbc:firebirdsql:embedded:" + mRelativeDatabasePath,
+                "SYSDBA", "masterkey");
+        connection.close();
+    }
+}
