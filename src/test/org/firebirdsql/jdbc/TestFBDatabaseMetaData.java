7b22de7589717728cbd25c82205b666b63291ef7
mrotteveel <mrotteveel@users.sourceforge.net>
2014-11-01 08:02:07 +0000

    FB 3 now has 50 system tables.

--- src/test/org/firebirdsql/jdbc/TestFBDatabaseMetaData.java
+++ src/test/org/firebirdsql/jdbc/TestFBDatabaseMetaData.java
@@ -178,7 +178,7 @@ public class TestFBDatabaseMetaData extends FBJUnit4TestBase {
             else if (databaseMajorVersion == 2 && databaseMinorVersion == 5)
                 sysTableCount = 42;
             else if (databaseMajorVersion == 3 && databaseMinorVersion == 0) {
-                sysTableCount = 47;
+                sysTableCount = 50;
             } else {
                 fail(String.format("Unsupported database server version %d.%d for this test case: found table count %d", databaseMajorVersion, databaseMinorVersion, count));
 
