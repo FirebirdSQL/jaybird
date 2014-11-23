7fed1c1374907458a65337b6f75a90475e166022
mrotteveel <mrotteveel@65644016-39b1-43b1-bf79-96bc8fe82c15>
2014-11-23 10:42:57 +0000

    JDBC-364 Incorrect use of WeakHashMap in FBManagedConnectionFactory and FBDriver

--- client-java/branches/Branch_2_2/src/main/org/firebirdsql/jdbc/FBDataSource.java
+++ client-java/branches/Branch_2_2/src/main/org/firebirdsql/jdbc/FBDataSource.java
@@ -1,7 +1,7 @@
 /*
  * $Id$
- *  
- * Firebird Open Source J2EE connector - JDBC driver
+ *
+ * Firebird Open Source JavaEE Connector - JDBC Driver
  *
  * Distributable under LGPL license.
  * You may obtain a copy of the License at http://www.gnu.org/copyleft/lgpl.html
@@ -14,16 +14,15 @@
  * This file was created by members of the firebird development team.
  * All individual contributions remain the Copyright (C) of those
  * individuals.  Contributors to this file are either listed here or
- * can be obtained from a CVS history command.
+ * can be obtained from a source control history command.
  *
  * All rights reserved.
  */
-
 package org.firebirdsql.jdbc;
 
-import java.io.Serializable;
-import java.sql.Connection;
-import java.sql.SQLException;
+import org.firebirdsql.ds.RootCommonDataSource;
+import org.firebirdsql.jca.FBConnectionRequestInfo;
+import org.firebirdsql.jca.FBManagedConnectionFactory;
 
 import javax.naming.Reference;
 import javax.resource.Referenceable;
@@ -31,10 +30,9 @@ import javax.resource.ResourceException;
 import javax.resource.spi.ConnectionManager;
 import javax.resource.spi.ManagedConnectionFactory;
 import javax.sql.DataSource;
-
-import org.firebirdsql.ds.RootCommonDataSource;
-import org.firebirdsql.jca.FBConnectionRequestInfo;
-import org.firebirdsql.jca.FBManagedConnectionFactory;
+import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 /**
  * The class <code>FBDataSource</code> is a ConnectionFactory for jdbc
@@ -47,9 +45,9 @@ public class FBDataSource extends RootCommonDataSource implements DataSource, Se
 
     private static final long serialVersionUID = 1178461472062969634L;
 
-    private ConnectionManager cm;
+    private final ConnectionManager cm;
 
-    private FBManagedConnectionFactory mcf;
+    private final FBManagedConnectionFactory mcf;
 
     private Reference jndiReference;
 
@@ -57,19 +55,19 @@ public class FBDataSource extends RootCommonDataSource implements DataSource, Se
 
     // this constructor is needed to make BES happy.
     public FBDataSource(ManagedConnectionFactory mcf, ConnectionManager cm) {
-        this((FBManagedConnectionFactory)mcf, cm);
+        this((FBManagedConnectionFactory) mcf, cm);
     }
-    
+
     public FBDataSource(FBManagedConnectionFactory mcf, ConnectionManager cm) {
         this.mcf = mcf;
         this.cm = cm;
     }
 
-
     /**
      * Set the JNDI <code>Reference</code> for this DataSource.
      *
-     * @param ref The JNDI reference for this DataSource
+     * @param ref
+     *         The JNDI reference for this DataSource
      */
     public void setReference(Reference ref) {
         this.jndiReference = ref;
@@ -84,79 +82,80 @@ public class FBDataSource extends RootCommonDataSource implements DataSource, Se
         return jndiReference;
     }
 
-
-  /**
-   * <p>Attempt to establish a database connection.
-   *
-   * @return  a Connection to the database
-   * @exception SQLException if a database-access error occurs.
-   */
-    public Connection getConnection() throws  SQLException {
+    /**
+     * <p>Attempt to establish a database connection.
+     *
+     * @return a Connection to the database
+     * @throws SQLException
+     *         if a database-access error occurs.
+     */
+    public Connection getConnection() throws SQLException {
         try {
-            return (Connection)cm.allocateConnection(mcf, mcf.getDefaultConnectionRequestInfo());
-        }
-        catch (ResourceException re) {
+            return (Connection) cm.allocateConnection(mcf, mcf.getDefaultConnectionRequestInfo());
+        } catch (ResourceException re) {
             throw new FBSQLException("Problem getting connection: " + re);
         }
     }
 
-
-  /**
-   * <p>Attempt to establish a database connection.
-   *
-   * @param username the database user on whose behalf the Connection is
-   *  being made
-   * @param password the user's password
-   * @return  a Connection to the database
-   * @exception SQLException if a database-access error occurs.
-   */
-    public Connection getConnection(String username, String password) throws  SQLException {
-        try 
-        {
-           //mcf makes a copy for us.
+    /**
+     * <p>Attempt to establish a database connection.
+     *
+     * @param username
+     *         the database user on whose behalf the Connection is
+     *         being made
+     * @param password
+     *         the user's password
+     * @return a Connection to the database
+     * @throws SQLException
+     *         if a database-access error occurs.
+     */
+    public Connection getConnection(String username, String password) throws SQLException {
+        try {
+            //mcf makes a copy for us.
             FBConnectionRequestInfo subjectCri = mcf.getDefaultConnectionRequestInfo();
             subjectCri.setUserName(username);
             subjectCri.setPassword(password);
-            return (Connection)cm.allocateConnection(mcf, subjectCri);
-        }
-        catch (ResourceException re) {
+            return (Connection) cm.allocateConnection(mcf, subjectCri);
+        } catch (ResourceException re) {
             throw new FBSQLException(re);
         }
     }
 
-  /**
-   * <p>Sets the maximum time in seconds that this data source will wait
-   * while attempting to connect to a database.  A value of zero
-   * specifies that the timeout is the default system timeout
-   * if there is one; otherwise it specifies that there is no timeout.
-   * When a DataSource object is created the login timeout is
-   * initially zero.
-   *
-   * @param seconds the data source login time limit
-   * @exception SQLException if a database access error occurs.
-   */
-    public void setLoginTimeout(int seconds) throws  SQLException {
+    /**
+     * <p>Sets the maximum time in seconds that this data source will wait
+     * while attempting to connect to a database.  A value of zero
+     * specifies that the timeout is the default system timeout
+     * if there is one; otherwise it specifies that there is no timeout.
+     * When a DataSource object is created the login timeout is
+     * initially zero.
+     *
+     * @param seconds
+     *         the data source login time limit
+     * @throws SQLException
+     *         if a database access error occurs.
+     */
+    public void setLoginTimeout(int seconds) throws SQLException {
         loginTimeout = seconds;
     }
 
-
-  /**
-   * Gets the maximum time in seconds that this data source can wait
-   * while attempting to connect to a database.  A value of zero
-   * means that the timeout is the default system timeout
-   * if there is one; otherwise it means that there is no timeout.
-   * When a DataSource object is created the login timeout is
-   * initially zero.
-   *
-   * @return the data source login time limit
-   * @exception SQLException if a database access error occurs.
-   */
-    public int getLoginTimeout() throws  SQLException {
+    /**
+     * Gets the maximum time in seconds that this data source can wait
+     * while attempting to connect to a database.  A value of zero
+     * means that the timeout is the default system timeout
+     * if there is one; otherwise it means that there is no timeout.
+     * When a DataSource object is created the login timeout is
+     * initially zero.
+     *
+     * @return the data source login time limit
+     * @throws SQLException
+     *         if a database access error occurs.
+     */
+    public int getLoginTimeout() throws SQLException {
         return loginTimeout;
     }
 
     // JDBC 4.0
-    
+
     public boolean isWrapperFor(Class<?> iface) throws SQLException {
         return iface != null && iface.isAssignableFrom(getClass());
     }
@@ -164,7 +163,7 @@ public class FBDataSource extends RootCommonDataSource implements DataSource, Se
     public <T> T unwrap(Class<T> iface) throws SQLException {
         if (!isWrapperFor(iface))
             throw new FBDriverNotCapableException();
-        
+
         return iface.cast(this);
     }
 
