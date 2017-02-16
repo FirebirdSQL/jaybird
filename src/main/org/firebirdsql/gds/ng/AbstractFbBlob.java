FirebirdSQL/jaybird

commit 3c632c8b7ec558713764725ea7e2a6d20099db5a

Use op_put_segment instead of op_batch_segments as it adds no benefit and is more complex
* Increase blob segment size to Short.MAX_VALUE
* Add specific optimizations for writing zero or space padding

src/main/org/firebirdsql/gds/ng/AbstractFbBlob.java
@@ -350,7 +350,6 @@ public abstract class AbstractFbBlob implements FbBlob, TransactionListener, Dat
     @Override
     public int getMaximumSegmentSize() {
         // TODO Max size in FB 3 is 2^16, not 2^15 - 1, is that for all versions, or only for newer protocols?
-        // Subtracting 2 because the maximum size includes length TODO: verify if true
-        return Short.MAX_VALUE - 2;
+        return Short.MAX_VALUE;
     }
 }

src/jna-client/org/firebirdsql/gds/ng/jna/JnaBlob.java
@@ -173,7 +173,6 @@ public class JnaBlob extends AbstractFbBlob implements FbBlob, DatabaseListener 
     @Override
     public void putSegment(byte[] segment) throws SQLException {
         try {
-            // TODO Handle exceeding max segment size?
             if (segment.length == 0) {
                 throw new FbExceptionBuilder().exception(jb_blobPutSegmentEmpty).toSQLException();
             }

src/main/org/firebirdsql/gds/impl/wire/XdrOutputStream.java
@@ -28,20 +28,17 @@ package org.firebirdsql.gds.impl.wire;
 import org.firebirdsql.encodings.Encoding;
 import org.firebirdsql.gds.ISCConstants;
 import org.firebirdsql.gds.ParameterBuffer;
-import org.firebirdsql.logging.Logger;
-import org.firebirdsql.logging.LoggerFactory;
 
+import javax.crypto.Cipher;
+import javax.crypto.CipherOutputStream;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.SecretKeySpec;
 import java.io.BufferedOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.util.Arrays;
-import java.security.NoSuchAlgorithmException;
 import java.security.InvalidKeyException;
-
-import javax.crypto.Cipher;
-import javax.crypto.spec.SecretKeySpec;
-import javax.crypto.CipherOutputStream;
-import javax.crypto.NoSuchPaddingException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 
 /**
  * An <code>XdrOutputStream</code> writes data in XDR format to an
@@ -59,17 +56,12 @@ public final class XdrOutputStream extends OutputStream {
 
     private static final int BUF_SIZE = 32767;
 
-    private static final Logger log = LoggerFactory.getLogger(XdrOutputStream.class);
-    private static final byte[] TEXT_PAD;
-    private static final byte[] ZERO_PADDING = new byte[3];
     public static final int SPACE_BYTE = 0x20;
     public static final int NULL_BYTE = 0x0;
-    static {
-        // fill the padding with blanks
-        byte[] textPad = new byte[BUF_SIZE];
-        Arrays.fill(textPad, (byte) SPACE_BYTE);
-        TEXT_PAD = textPad;
-    }
+    private static final int TEXT_PAD_LENGTH = BUF_SIZE;
+    private static final byte[] TEXT_PAD = createPadding(BUF_SIZE, SPACE_BYTE);
+    private static final int ZERO_PAD_LENGTH = 3;
+    private static final byte[] ZERO_PADDING = new byte[ZERO_PAD_LENGTH];
 
     private OutputStream out;
 
@@ -117,15 +109,43 @@ public final class XdrOutputStream extends OutputStream {
     }
 
     /**
+     * Writes zero padding of the specified length
+     *
+     * @param length Length to write
+     * @throws IOException if an error occurs while writing to the underlying output stream
+     * @see #writePadding(int, int)
+     */
+    public void writeZeroPadding(int length) throws IOException {
+        byte[] padding = length <= ZERO_PAD_LENGTH ? ZERO_PADDING : new byte[length];
+        out.write(padding, 0, length);
+    }
+
+    /**
+     * Writes space ({@code 0x20}) padding of the specified length
+     *
+     * @param length Length to write
+     * @throws IOException if an error occurs while writing to the underlying output stream
+     * @see #writePadding(int, int)
+     */
+    public void writeSpacePadding(int length) throws IOException {
+        byte[] padding = length <= TEXT_PAD_LENGTH ? TEXT_PAD : createPadding(length, SPACE_BYTE);
+        out.write(padding, 0, length);
+    }
+
+    /**
      * Writes padding for the specified length of the specified padding byte.
+     * <p>
+     * Prefer using the more specific {@link #writeZeroPadding(int)} and {@link #writeZeroPadding(int)}.
+     * </p>
      *
      * @param length
      *         Length of padding to write
      * @param padByte
      *         Padding byte to use
      * @throws IOException
-     *         if an error occurs while writing to the
-     *         underlying output stream
+     *         if an error occurs while writing to the underlying output stream
+     * @see #writeSpacePadding(int)
+     * @see #writeZeroPadding(int)
      */
     public void writePadding(int length, int padByte) throws IOException {
         final byte[] padding;
@@ -134,12 +154,22 @@ public final class XdrOutputStream extends OutputStream {
         } else if (padByte == NULL_BYTE && length <= ZERO_PADDING.length) {
             padding = ZERO_PADDING;
         } else {
-            padding = new byte[length];
-            if (padByte != NULL_BYTE) {
-                Arrays.fill(padding, (byte) padByte);
-            }
+            padding = createPadding(length, (byte) padByte);
         }
         out.write(padding, 0, length);
+    }
+
+    /**
+     * Creates a padding array.
+     *
+     * @param length Length of array
+     * @param padByte Byte value for filling the array
+     * @return Array filled with {@code padByte}
+     */
+    private static byte[] createPadding(int length, int padByte) {
+        byte[] padding = new byte[length];
+        Arrays.fill(padding, (byte) padByte);
+        return padding;
     }
 
     /**
@@ -158,29 +188,6 @@ public final class XdrOutputStream extends OutputStream {
             writeInt(len);
             write(buffer, 0, len, (4 - len) & 3);
         }
-    }
-
-    /**
-     * Write a blob buffer to the underlying output stream in XDR format.
-     *
-     * @param buffer A <code>byte</code> array containing the blob
-     * @throws IOException if an error occurs while writing to the
-     *         underlying output stream
-     */
-    public void writeBlobBuffer(byte[] buffer) throws IOException {
-        int len = buffer.length; // 2 for short for buffer length
-        if (log != null && log.isDebugEnabled()) log.debug("writeBlobBuffer len: " + len);
-        if (len > Short.MAX_VALUE) {
-            throw new IOException(""); //Need a value???
-        }
-        // TODO This is probably wrong. It looks like op_batch_segments allows for writing multiple segments up to 2^64 (or 2^63 - 1?) in length (including all segment sizes)
-        // writeLong(len + 2); // This works as well instead of 2 * writeInt(len + 2); below
-        writeInt(len + 2);
-        writeInt(len + 2); //bizarre but true! three copies of the length
-        write(len & 0xff);
-        write((len >> 8) & 0xff);
-        // TODO 4 - len + 2 or 4 - (len + 2) ?
-        write(buffer, 0, len, ((4 - len + 2) & 3));
     }
 
     /**
@@ -274,7 +281,7 @@ public final class XdrOutputStream extends OutputStream {
     public void write(byte[] b, int offset, int len, int pad) throws IOException {
         out.write(b, offset, len);
         // TODO We shouldn't always pad with spaces
-        writePadding(pad, SPACE_BYTE);
+        writeSpacePadding(pad);
     }
 
     /**

src/main/org/firebirdsql/gds/ng/wire/version10/V10OutputBlob.java
@@ -30,6 +30,7 @@ import java.io.IOException;
 import java.sql.SQLException;
 
 import static org.firebirdsql.gds.JaybirdErrorCodes.jb_blobPutSegmentEmpty;
+import static org.firebirdsql.gds.JaybirdErrorCodes.jb_blobPutSegmentTooLong;
 import static org.firebirdsql.gds.impl.wire.WireProtocolConstants.*;
 
 /**
@@ -94,9 +95,12 @@ public class V10OutputBlob extends AbstractFbWireOutputBlob implements FbWireBlo
     @Override
     public void putSegment(byte[] segment) throws SQLException {
         try {
-            // TODO Handle exceeding max segment size?
             if (segment.length == 0) {
                 throw new FbExceptionBuilder().exception(jb_blobPutSegmentEmpty).toSQLException();
+            }
+            // TODO Handle by performing multiple puts?
+            if (segment.length > getMaximumSegmentSize()) {
+                throw new FbExceptionBuilder().exception(jb_blobPutSegmentTooLong).toSQLException();
             }
             synchronized (getSynchronizationObject()) {
                 checkDatabaseAttached();
@@ -106,11 +110,10 @@ public class V10OutputBlob extends AbstractFbWireOutputBlob implements FbWireBlo
                 final FbWireDatabase database = getDatabase();
                 try {
                     final XdrOutputStream xdrOut = database.getXdrStreamAccess().getXdrOut();
-                    // TODO Using op_batch_segments over op_put_segment doesn't seem to provide a real benefit in current implementation (see XdrOutputStream)
-                    // TODO Is there any actual benefit possible, like sending multiple segments of maximum size?
-                    xdrOut.writeInt(op_batch_segments);
+                    xdrOut.writeInt(op_put_segment);
                     xdrOut.writeInt(getHandle());
-                    xdrOut.writeBlobBuffer(segment);
+                    xdrOut.writeInt(segment.length);
+                    xdrOut.writeBuffer(segment);
                     xdrOut.flush();
                 } catch (IOException e) {
                     throw new FbExceptionBuilder().exception(ISCConstants.isc_net_write_err).cause(e).toSQLException();

src/main/org/firebirdsql/gds/ng/wire/version10/V10Statement.java
@@ -598,7 +598,7 @@ public class V10Statement extends AbstractFbWireStatement implements FbWireState
             if (buffer != null) {
                 xdrOut.write(buffer, 0, -len);
             } else {
-                xdrOut.writePadding(-len, 0x00);
+                xdrOut.writeZeroPadding(-len);
             }
         } else {
             // decrement length because it was incremented before
@@ -610,10 +610,10 @@ public class V10Statement extends AbstractFbWireStatement implements FbWireState
                     xdrOut.write(buffer, 0, len, (4 - len) & 3);
                 } else {
                     xdrOut.write(buffer, 0, buflen, 0);
-                    xdrOut.writePadding(len - buflen + ((4 - len) & 3), 0x20);
+                    xdrOut.writeSpacePadding(len - buflen + ((4 - len) & 3));
                 }
             } else {
-                xdrOut.writePadding(len + ((4 - len) & 3), 0x20);
+                xdrOut.writeSpacePadding(len + ((4 - len) & 3));
             }
         }
     }

src/test/org/firebirdsql/gds/ng/BaseTestBlob.java
@@ -346,19 +346,14 @@ public abstract class BaseTestBlob extends FBJUnit4TestBase {
      * @throws SQLException
      */
     protected boolean validateBlob(int id, byte[] baseContent, int requiredSize) throws SQLException {
-        Connection con = getConnectionViaDriverManager();
-        CallableStatement cstmt = null;
-        try {
-            cstmt = con.prepareCall(EXECUTE_CHECK_BINARY_BLOB);
+        try (Connection con = getConnectionViaDriverManager();
+            CallableStatement cstmt = con.prepareCall(EXECUTE_CHECK_BINARY_BLOB)) {
             cstmt.setInt(1, id);
             cstmt.setBytes(2, baseContent);
             cstmt.setInt(3, requiredSize);
 
             cstmt.execute();
             return cstmt.getBoolean(1);
-        } finally {
-            closeQuietly(cstmt);
-            closeQuietly(con);
         }
     }
 

src/test/org/firebirdsql/jdbc/TestFBBlob.java
@@ -359,6 +359,7 @@ public class TestFBBlob extends FBJUnit4TestBase {
     @Test
     public void testBlobCloseOnCommit() throws Exception {
         Connection connection = getConnectionViaDriverManager();
+        //noinspection TryFinallyCanBeTryWithResources
         try {
             populateBlob(connection, new byte[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 });
 
@@ -424,7 +425,7 @@ public class TestFBBlob extends FBJUnit4TestBase {
     @Test
     public void testBlobCloseOnConnectionClose_inAutoCommit() throws Exception {
         Connection connection = getConnectionViaDriverManager();
-        OutputStream binaryStream = null;
+        OutputStream binaryStream;
         //noinspection TryFinallyCanBeTryWithResources
         try {
             FBBlob blob = (FBBlob) connection.createBlob();

commit b5a748642b37896ca6703350a0599a307c51f29d

Fix bug in 2.5 and earlier introduced by 3c632c8

src/main/org/firebirdsql/gds/ng/AbstractFbBlob.java
@@ -350,6 +350,6 @@ public abstract class AbstractFbBlob implements FbBlob, TransactionListener, Dat
     @Override
     public int getMaximumSegmentSize() {
         // TODO Max size in FB 3 is 2^16, not 2^15 - 1, is that for all versions, or only for newer protocols?
-        return Short.MAX_VALUE;
+        return Short.MAX_VALUE - 2;
     }
 }