38b4907d02e0944da0730f06ee59989b342539dc
Mark Rotteveel <mark@lawinegevaar.nl>
2018-06-24 16:27:06 +0200

    Fix unintentional license change caused by move action

--- src/main/org/firebirdsql/gds/ng/wire/auth/legacy/UnixCrypt.java
+++ src/main/org/firebirdsql/gds/ng/wire/auth/legacy/UnixCrypt.java
@@ -1,20 +1,12 @@
 /*
- * Firebird Open Source JavaEE Connector - JDBC Driver
+ * @(#)UnixCrypt.java	0.9 96/11/25
  *
- * Distributable under LGPL license.
- * You may obtain a copy of the License at http://www.gnu.org/copyleft/lgpl.html
+ * Copyright (c) 1996 Aki Yoshida. All rights reserved.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * LGPL License for more details.
- *
- * This file was created by members of the firebird development team.
- * All individual contributions remain the Copyright (C) of those
- * individuals.  Contributors to this file are either listed here or
- * can be obtained from a source control history command.
- *
- * All rights reserved.
+ * Permission to use, copy, modify and distribute this software
+ * for non-commercial or commercial purposes and without fee is
+ * hereby granted provided that this copyright notice appears in
+ * all copies.
  */
 
 /**
