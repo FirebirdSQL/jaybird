3760a49b6a95e252e9f1e54059225f7751701e4f
Mark Rotteveel <mark@lawinegevaar.nl>
2018-09-13 16:43:21 +0200

    Update Ant to 1.9.13

--- bin/complete-ant-cmd.pl
+++ bin/complete-ant-cmd.pl
@@ -15,10 +15,10 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 #
-# A script to allow Bash or Z-Shell to complete an Ant command-line.  
+# A script to allow Bash or Z-Shell to complete an Ant command-line.
 #
 # To install for Bash 2.0 or better, add the following to ~/.bashrc:
-# 
+#
 #     complete -C complete-ant-cmd.pl ant build.sh
 #
 # To install for Z-Shell 2.5 or better, add the following to ~/.zshrc:
@@ -30,20 +30,18 @@
 #         set -A reply $(COMP_LINE=$args_line complete-ant-cmd.pl ${args[1]} $1)
 #     }
 #     compctl -K ant_complete ant build.sh
-#     
-# @author Mike Williams <mikew@cortexebusiness.com.au>
 
-my $cmdLine = $ENV{'COMP_LINE'};
+my $cmdLine = "$ENV{'ANT_ARGS'} $ENV{'COMP_LINE'}";
 my $antCmd = $ARGV[0];
 my $word = $ARGV[1];
 
 my @completions;
 if ($word =~ /^-/) {
-    list( restrict( $word, getArguments() ));
-} elsif ($cmdLine =~ /-(f|buildfile)\s+\S*$/) {
-    list( getBuildFiles($word) );
+    list(restrict($word, getArguments()));
+} elsif ($cmdLine =~ /-(f|file|buildfile)\s+\S*$/) {
+    list(getBuildFiles($word));
 } else {
-    list( restrict( $word, getTargets() ));
+    list(restrict($word, getTargets()));
 }
 
 exit(0);
@@ -56,59 +54,61 @@ sub list {
 
 sub restrict {
     my ($word, @completions) = @_;
-    grep( /^\Q$word\E/, @completions );
+    grep(/^\Q$word\E/, @completions);
 }
 
 sub getArguments {
-    qw(-buildfile -debug -emacs -f -find -help -listener -logfile 
-       -logger -projecthelp -quiet -verbose -version); 
+    qw(-buildfile -debug -emacs -f -file -find -help -listener -logfile
+       -logger -projecthelp -quiet -verbose -version);
 }
 
 
 sub getBuildFiles {
     my ($word) = @_;
-    grep( /\.xml$/, glob( "$word*" ));
+    grep(/\.xml$/, glob("$word*"));
 }
 
 sub getTargets {
 
     # Look for build-file
     my $buildFile = 'build.xml';
-    if ($cmdLine =~ /-(f|buildfile)\s+(\S+)/) {
+    if ($cmdLine =~ /-(f|file|buildfile)\s+(\S+)(?!.*\s-(f|file|buildfile)\s)/) {
         $buildFile = $2;
     }
     return () unless (-f $buildFile);
 
-    # Run "ant -projecthelp" to list targets.  Keep a cache of results in a
-    # cache-file.
+    # Run "ant -projecthelp -debug" to list targets (-debug is required to get
+    # "Other targets", i.e. targets without a description).  Keep a cache of
+    # results in a cache-file.
     my $cacheFile = $buildFile;
     $cacheFile =~ s|(.*/)?(.*)|${1}.ant-targets-${2}|;
     if ((!-e $cacheFile) || (-z $cacheFile) || (-M $buildFile) < (-M $cacheFile)) {
-        open( CACHE, '>'.$cacheFile ) || die "can\'t write $cacheFile: $!\n";
-        open( HELP, "$antCmd -projecthelp -f '$buildFile'|" ) || return(); 
+        open(CACHE, '>' . $cacheFile) || die "can\'t write $cacheFile: $!\n";
+        open(HELP, "$antCmd -projecthelp -debug -buildfile '$buildFile'|") || return();
         my %targets;
-        while( <HELP> ) {
-            if (/^\s+(\S+)/) {
+        while (<HELP>) {
+            # Exclude target names starting with dash, because they cannot be
+            # specified on the command line.
+            if (/^\s+\+Target:\s+(?!-)(\S+)/) {
                 $targets{$1}++;
             }
         }
         my @targets = sort keys %targets;
-        for (@targets) { print CACHE "$_\n"; }
+        for (@targets) {
+            print CACHE "$_\n";
+        }
         return @targets;
     }
-    
+
     # Read the target-cache
-    open( CACHE, $cacheFile ) || die "can\'t read $cacheFile: $!\n";
+    open(CACHE, $cacheFile) || die "can\'t read $cacheFile: $!\n";
     my @targets;
     while (<CACHE>) {
         chop;
         s/\r$//;  # for Cygwin
-        push( @targets, $_ );
+        push(@targets, $_);
     }
-    close( CACHE );
+    close(CACHE);
     @targets;
 
 }
-
-
-
