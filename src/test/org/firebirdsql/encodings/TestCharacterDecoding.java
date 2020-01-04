6a26d12356a40a359ec9199d02056565585ecad1
Mark Rotteveel <mark@lawinegevaar.nl>
2020-01-04 17:11:52 +0100

    Eliminate some todos

--- src/test/org/firebirdsql/encodings/TestCharacterDecoding.java
+++ src/test/org/firebirdsql/encodings/TestCharacterDecoding.java
@@ -22,27 +22,14 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-// TODO Needs a better name as it no longer tests character translation
-public class TestCharacterTranslation {
-
-    private static final IEncodingFactory factory = EncodingFactory.getPlatformDefault();
-
-    private byte[] testBytes = new byte[] {
-            0x61, 0x62, 0x63
-    };
-
-    private char[] originalChars = new char[] {
-            '\u0061', '\u0062', '\u0063'
-    };
+public class TestCharacterDecoding {
 
     @Test
     public void testOriginal() {
+        IEncodingFactory factory = EncodingFactory.getPlatformDefault();
         Encoding encoding = factory.getEncodingForFirebirdName("ISO8859_1");
-
-        String testStr = encoding.decodeFromCharset(testBytes);
-        String checkStr = new String(originalChars);
-
-        assertTrue("Strings should be equal", testStr.equals(checkStr));
+        String testStr = encoding.decodeFromCharset(new byte[] { 0x61, 0x62, 0x63 });
+        assertEquals("Strings should be equal", "\u0061\u0062\u0063", testStr);
     }
 
 }
