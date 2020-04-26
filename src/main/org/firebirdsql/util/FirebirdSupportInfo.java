b0d4fdcf1f162cad3a3fbd5c49ec853e0978a7d1
Mark Rotteveel <mark@lawinegevaar.nl>
2020-04-26 16:20:43 +0200

    Update system table count for Firebird 4

--- src/main/org/firebirdsql/util/FirebirdSupportInfo.java
+++ src/main/org/firebirdsql/util/FirebirdSupportInfo.java
@@ -505,7 +505,7 @@ public final class FirebirdSupportInfo {
         } else if (databaseMajorVersion == 3 && databaseMinorVersion == 0) {
             return 50;
         } else if (databaseMajorVersion == 4 && databaseMinorVersion == 0) {
-            return 51;
+            return 53;
         } else {
             return -1;
         }
