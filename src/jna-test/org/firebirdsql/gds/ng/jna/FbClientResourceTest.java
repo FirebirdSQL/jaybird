7d1d4c0306f04f56e0338e0dc82353308ce62909
Mark Rotteveel <mark@lawinegevaar.nl>
2020-08-11 15:07:05 +0200

    Fix test not working when run in isolation

--- src/jna-test/org/firebirdsql/gds/ng/jna/FbClientResourceTest.java
+++ src/jna-test/org/firebirdsql/gds/ng/jna/FbClientResourceTest.java
@@ -26,20 +26,25 @@ import org.junit.Test;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
+import static java.util.Objects.requireNonNull;
+
 public class FbClientResourceTest {
 
     @ClassRule
     public static final GdsTypeRule typeRule = GdsTypeRule.supportsNativeOnly();
 
     /**
-     * Rationale: due to code changes, the impose implementation didn't work but instead logged an exception
+     * Rationale: due to code changes, the dispose implementation didn't work but instead logged an exception
      */
     @Test
     public void testDisposeImplementation() throws Exception {
         AbstractNativeDatabaseFactory factory = (AbstractNativeDatabaseFactory) FBTestProperties.getFbDatabaseFactory();
+        // Call to ensure resource is initialized
+        factory.getClientLibrary();
         Field resourceField = AbstractNativeDatabaseFactory.class.getDeclaredField("resource");
         resourceField.setAccessible(true);
-        FbClientResource resource = (FbClientResource) resourceField.get(factory);
+        FbClientResource resource = (FbClientResource) requireNonNull(resourceField.get(factory),
+                "Test setup incomplete, resource not initialized");
         Method disposeImpl = FbClientResource.class.getDeclaredMethod("disposeImpl");
         disposeImpl.setAccessible(true);
 
