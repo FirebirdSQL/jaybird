184c865665f26301bfaa29437425d482196ff1a1
Mark Rotteveel <mark@lawinegevaar.nl>
2022-12-24 17:32:38 +0100

    Remove deprecated from package-info.java

--- src/openoffice/org/firebirdsql/jdbc/oo/package-info.java
+++ src/openoffice/org/firebirdsql/jdbc/oo/package-info.java
@@ -1,7 +1,4 @@
 /**
  * JDBC implementation classes specifically for the OOREMOTE protocol.
- *
- * @deprecated Switch to using "Firebird External" in LibreOffice, will be removed in Jaybird 6.
  */
-@Deprecated
 package org.firebirdsql.jdbc.oo;
