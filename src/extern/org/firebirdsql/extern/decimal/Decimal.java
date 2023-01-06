e1f87162b7fe4bb73ff39d275255f3f6ccd87358
Mark Rotteveel <mark@lawinegevaar.nl>
2023-01-06 12:13:42 +0100

    Sync sources with decimal-java 1.0.1

--- src/extern/org/firebirdsql/extern/decimal/Decimal.java
+++ src/extern/org/firebirdsql/extern/decimal/Decimal.java
@@ -375,8 +375,7 @@ public abstract class Decimal<T extends Decimal<T>> {
                 return getSpecialConstant(Signum.NEGATIVE, DecimalType.INFINITY);
             }
 
-            // TODO Use new BigDecimal(double, MathContext) instead, has slightly different precision?
-            return valueOf(new BigDecimal(Double.toString(value), getMathContext()), overflowHandling);
+            return valueOf(BigDecimal.valueOf(value).round(getMathContext()), overflowHandling);
         }
 
         /**
