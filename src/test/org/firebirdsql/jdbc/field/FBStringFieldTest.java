74343cb54d258d91cbb067cd43c4d1322d185911
Mark Rotteveel <mark@lawinegevaar.nl>
2025-03-04 14:26:24 +0100

    #836 Simplify code of IOUtils
    (cherry picked from commit 64d0249ce0f28693ab91d7294174d80d788caf66)

--- src/test/org/firebirdsql/jdbc/field/FBStringFieldTest.java
+++ src/test/org/firebirdsql/jdbc/field/FBStringFieldTest.java
@@ -138,7 +138,7 @@ class FBStringFieldTest extends BaseJUnit5TestFBField<FBStringField, String> {
     @Override
     void getBinaryStreamNonNull() throws Exception {
         toReturnStringExpectations(TEST_STRING_SHORT, encoding);
-        String fromStream = new String(IOUtils.toBytes(field.getBinaryStream(), Integer.MAX_VALUE));
+        String fromStream = new String(field.getBinaryStream().readAllBytes());
 
         assertEquals(TEST_STRING_SHORT, fromStream.trim(), "Binary stream values test failure");
     }
@@ -147,7 +147,7 @@ class FBStringFieldTest extends BaseJUnit5TestFBField<FBStringField, String> {
     @Override
     void getObject_InputStream() throws Exception {
         toReturnStringExpectations(TEST_STRING_SHORT, encoding);
-        String fromStream = new String(IOUtils.toBytes(field.getObject(InputStream.class), Integer.MAX_VALUE));
+        String fromStream = new String(field.getObject(InputStream.class).readAllBytes());
 
         assertEquals(TEST_STRING_SHORT, fromStream.trim(), "Binary stream values test failure");
     }
@@ -315,7 +315,7 @@ class FBStringFieldTest extends BaseJUnit5TestFBField<FBStringField, String> {
     void getCharacterStreamNonNull() throws Exception {
         toReturnStringExpectations(TEST_STRING_SHORT, encoding);
 
-        String value = IOUtils.toString(field.getCharacterStream(), Integer.MAX_VALUE);
+        String value = IOUtils.toString(field.getCharacterStream(), -1);
         assertEquals(TEST_STRING_SHORT, value, "Unexpected value from getCharacterStream");
     }
 
@@ -324,7 +324,7 @@ class FBStringFieldTest extends BaseJUnit5TestFBField<FBStringField, String> {
     void getObject_Reader() throws Exception {
         toReturnStringExpectations(TEST_STRING_SHORT, encoding);
 
-        String value = IOUtils.toString(field.getObject(Reader.class), Integer.MAX_VALUE);
+        String value = IOUtils.toString(field.getObject(Reader.class), -1);
         assertEquals(TEST_STRING_SHORT, value, "Unexpected value from getCharacterStream");
     }
 
