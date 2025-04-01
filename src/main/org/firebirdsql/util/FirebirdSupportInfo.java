ca2a7d4a7c66de659671ddf6b67cd5fabc8a010c
Mark Rotteveel <mark@lawinegevaar.nl>
2025-04-01 12:34:42 +0200

    Touch copyright year after previous commit

--- src/main/org/firebirdsql/util/FirebirdSupportInfo.java
+++ src/main/org/firebirdsql/util/FirebirdSupportInfo.java
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: Copyright 2014-2024 Mark Rotteveel
+// SPDX-FileCopyrightText: Copyright 2014-2025 Mark Rotteveel
 // SPDX-License-Identifier: LGPL-2.1-or-later
 package org.firebirdsql.util;
 
