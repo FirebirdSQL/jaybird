0ccf3ddc91261cc9c66b1ffed4addf15cf8d3b81
Mark Rotteveel <mark@lawinegevaar.nl>
2025-04-06 12:39:20 +0200

    Make Version19Descriptor inherit from Version18Descriptor

--- src/main/org/firebirdsql/gds/ng/wire/version19/Version19Descriptor.java
+++ src/main/org/firebirdsql/gds/ng/wire/version19/Version19Descriptor.java
@@ -2,25 +2,10 @@
 // SPDX-License-Identifier: LGPL-2.1-or-later
 package org.firebirdsql.gds.ng.wire.version19;
 
-import org.firebirdsql.encodings.Encoding;
-import org.firebirdsql.gds.BlobParameterBuffer;
-import org.firebirdsql.gds.ServiceParameterBuffer;
-import org.firebirdsql.gds.ServiceRequestBuffer;
-import org.firebirdsql.gds.impl.ServiceParameterBufferImp;
-import org.firebirdsql.gds.impl.ServiceRequestBufferImp;
 import org.firebirdsql.gds.impl.wire.WireProtocolConstants;
-import org.firebirdsql.gds.ng.ParameterConverter;
-import org.firebirdsql.gds.ng.TransactionState;
 import org.firebirdsql.gds.ng.WarningMessageCallback;
 import org.firebirdsql.gds.ng.wire.*;
-import org.firebirdsql.gds.ng.wire.version10.V10AsynchronousChannel;
-import org.firebirdsql.gds.ng.wire.version10.V10Service;
-import org.firebirdsql.gds.ng.wire.version10.V10Transaction;
-import org.firebirdsql.gds.ng.wire.version11.V11InputBlob;
-import org.firebirdsql.gds.ng.wire.version11.V11OutputBlob;
-import org.firebirdsql.gds.ng.wire.version13.V13ParameterConverter;
-
-import java.sql.SQLException;
+import org.firebirdsql.gds.ng.wire.version18.Version18Descriptor;
 
 /**
  * The {@link ProtocolDescriptor} for the Firebird version 19 protocol. This version applies to Firebird 5.0.3, but also
@@ -29,10 +14,10 @@ import java.sql.SQLException;
  * @author Mark Rotteveel
  * @since 7
  */
-public final class Version19Descriptor extends AbstractProtocolDescriptor implements ProtocolDescriptor {
+public class Version19Descriptor extends Version18Descriptor implements ProtocolDescriptor {
 
     public Version19Descriptor() {
-        super(
+        this(
                 WireProtocolConstants.PROTOCOL_VERSION19,
                 WireProtocolConstants.arch_generic,
                 WireProtocolConstants.ptype_lazy_send, // Protocol implementation expects lazy send
@@ -41,59 +26,19 @@ public final class Version19Descriptor extends AbstractProtocolDescriptor implem
                 8);
     }
 
+    protected Version19Descriptor(int version, int architecture, int minimumType, int maximumType,
+            boolean supportsWireCompression, int weight) {
+        super(version, architecture, minimumType, maximumType, supportsWireCompression, weight);
+    }
+
     @Override
     public FbWireDatabase createDatabase(WireDatabaseConnection connection) {
         return new V19Database(connection, this);
     }
 
     @Override
-    public FbWireService createService(WireServiceConnection connection) {
-        return new V10Service(connection, this);
-    }
-
-    @Override
-    public ServiceParameterBuffer createServiceParameterBuffer(final WireServiceConnection connection) {
-        final Encoding stringEncoding = connection.getEncodingFactory().getEncodingForFirebirdName("UTF8");
-        return new ServiceParameterBufferImp(ServiceParameterBufferImp.SpbMetaData.SPB_VERSION_2, stringEncoding);
-    }
-
-    @Override
-    public ServiceRequestBuffer createServiceRequestBuffer(final WireServiceConnection connection) {
-        final Encoding stringEncoding = connection.getEncodingFactory().getEncodingForFirebirdName("UTF8");
-        return new ServiceRequestBufferImp(ServiceRequestBufferImp.SrbMetaData.SRB_VERSION_2, stringEncoding);
-    }
-
-    @Override
-    public FbWireTransaction createTransaction(final FbWireDatabase database, final int transactionHandle,
-            final TransactionState initialState) {
-        return new V10Transaction(database, transactionHandle, initialState);
-    }
-
-    @Override
     public FbWireStatement createStatement(final FbWireDatabase database) {
         return new V19Statement(database);
-    }
-
-    @Override
-    public FbWireBlob createOutputBlob(FbWireDatabase database, FbWireTransaction transaction,
-            BlobParameterBuffer blobParameterBuffer) throws SQLException {
-        return new V11OutputBlob(database, transaction, blobParameterBuffer);
-    }
-
-    @Override
-    public FbWireBlob createInputBlob(FbWireDatabase database, FbWireTransaction transaction,
-            BlobParameterBuffer blobParameterBuffer, long blobId) throws SQLException {
-        return new V11InputBlob(database, transaction, blobParameterBuffer, blobId);
-    }
-
-    @Override
-    public FbWireAsynchronousChannel createAsynchronousChannel(FbWireDatabase database) {
-        return new V10AsynchronousChannel(database);
-    }
-
-    @Override
-    protected ParameterConverter<WireDatabaseConnection, WireServiceConnection> getParameterConverter() {
-        return new V13ParameterConverter();
     }
 
     @Override
