38d47ce52e4ff559cf337e93774f7f3cca0ea404
Mark Rotteveel <mark@lawinegevaar.nl>
2026-01-22 16:28:08 +0100

    #906 JDBC 4.5 compat: use type code 2016 for DECFLOAT

--- src/main/org/firebirdsql/jdbc/JaybirdType.java
+++ src/main/org/firebirdsql/jdbc/JaybirdType.java
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: Copyright 2018-2022 Mark Rotteveel
+// SPDX-FileCopyrightText: Copyright 2018-2026 Mark Rotteveel
 // SPDX-License-Identifier: LGPL-2.1-or-later OR BSD-3-Clause
 package org.firebirdsql.jdbc;
 
@@ -14,7 +14,14 @@ import java.sql.SQLType;
 @Volatile(reason = "Defined types may receive a different type number when standardized in JDBC")
 public enum JaybirdType implements SQLType {
 
-    @Volatile(reason = "To be standardized by future version of JDBC, will be deprecated")
+    /**
+     * Type for {@code DECFLOAT}.
+     * <p>
+     * When using Java 26 or higher, use {@code java.sql.JDBCType.DECFLOAT}. This constant might be deprecated and
+     * removed once Jaybird only supports versions after Java 26.
+     * </p>
+     */
+    @Volatile(reason = "Prefer java.sql.JDBCType.DECFLOAT when using Java 26 or higher")
     DECFLOAT(JaybirdTypeCodes.DECFLOAT);
 
     private final Integer type;
