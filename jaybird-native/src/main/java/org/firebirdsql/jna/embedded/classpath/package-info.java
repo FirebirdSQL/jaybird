d104f3d0926bdb48352778d63cc787fc64548fde
Mark Rotteveel <mark@lawinegevaar.nl>
2026-02-14 12:26:21 +0100

    Add nullability to org.firebirdsql.jna.embedded.classpath

--- jaybird-native/src/main/java/org/firebirdsql/jna/embedded/classpath/package-info.java
+++ jaybird-native/src/main/java/org/firebirdsql/jna/embedded/classpath/package-info.java
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: Copyright 2022 Mark Rotteveel
+// SPDX-FileCopyrightText: Copyright 2022-2026 Mark Rotteveel
 // SPDX-License-Identifier: LGPL-2.1-or-later
 /**
  * Supporting classes for implementations of {@link org.firebirdsql.jna.embedded.spi} to provide Firebird Embedded on
@@ -6,4 +6,7 @@
  *
  * @since 5
  */
+@NullMarked
 package org.firebirdsql.jna.embedded.classpath;
+
+import org.jspecify.annotations.NullMarked;
